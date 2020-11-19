c0a7e9a49b6026563a6715db6aa6af7310be8034
Jorge Pabon <jmpabon@gmail.com>
2020-11-19 16:39:38 +0800

    Added: ouisynclistitem.dart
    Contains:
    
    - OuiSyncListItem class, which extends StatelessWidget. It is used in the ListView builder, on the homepage, providing the layout for the items, according with its type (folder, file, etc.).
    
    - _FolderDescription class, which extends StatelessWidget. It provides the layout for the description of a folder.
    
    - _FileDescription class, which extends StatelessWidget. It provides the layout for the description of a file.

---
+++ lib/app/controls/ouisynclistitem.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ouisync_app/app/models/baseitem.dart';
+import 'package:ouisync_app/app/models/ouisyncfile.dart';
+import 'package:ouisync_app/app/models/ouisyncfolder.dart';
+
+class OuiSyncListItem extends StatelessWidget {
+  const OuiSyncListItem({
+    this.itemData,
+  });
+
+  final BaseItem itemData;
+
+  @override
+  Widget build(BuildContext context) {
+    final row = Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        itemData.icon,
+        Expanded(
+            flex: 1,
+            child: itemData.type == OSType.folder
+                ? _FolderDescription(folderData: itemData)
+                : _FileDescription(fileData: itemData)
+        ),
+        itemData.type == OSType.folder
+            ? const Icon(Icons.arrow_forward_ios, size: 16.0,)
+            : const Icon(Icons.more_vert, size: 24.0,),
+      ],
+    );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: itemData.type == OSType.folder
+          ? Card(margin: EdgeInsets.symmetric(vertical: 5.0), child: row)
+          : row,
+    );
+  }
+
+}
+
+class _FolderDescription extends StatelessWidget {
+  const _FolderDescription({
+    Key key,
+    this.folderData,
+  }) : super(key: key);
+
+  final OuiSyncFolder folderData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            folderData.name,
+            style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 16.0,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
+          Text(
+            folderData.status,
+            style: const TextStyle(fontSize: 12.0),
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
+          Text(
+            '${folderData.items.length} objects',
+            style: const TextStyle(fontSize: 12.0),
+          ),
+          Text(
+              folderData.location.length == 0
+                  ? "-"
+                  : folderData.location.join("/"),
+              style: const TextStyle(fontSize: 12.0)
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _FileDescription extends StatelessWidget {
+  const _FileDescription({
+    Key key,
+    this.fileData,
+  }) : super(key: key);
+
+  final OuiSyncFile fileData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            this.fileData.name,
+            style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 16.0,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
+          Text(
+            this.fileData.status,
+            style: const TextStyle(fontSize: 12.0),
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
+          Text(
+              this.fileData.location.length == 0
+                  ? "-"
+                  : this.fileData.location.join("/"),
+              style: const TextStyle(fontSize: 12.0)
+          )
+        ],
+      ),
+    );
+  }
+}
