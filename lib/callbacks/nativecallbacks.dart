ac20b94ad2da6ecbd737354ab349bf063c853162
Jorge Pabon <jmpabon@gmail.com>
2020-12-04 07:40:49 -0500

    Static functions temporarily commented

--- lib/callbacks/nativecallbacks.dart
+++ lib/callbacks/nativecallbacks.dart
@@ -3,16 +3,16 @@ import 'dart:ffi';
 import 'dart:io';
 import 'package:isolate/ports.dart';
 
-final DynamicLibrary nativeAddLib = Platform.isAndroid
-    ? DynamicLibrary.open("libnative_add.so")
+final DynamicLibrary ouisyncLib = Platform.isAndroid
+    ? DynamicLibrary.open("libouisync.os")
     : DynamicLibrary.process();
 
-final int Function(int x, int y) nativeAdd =
-nativeAddLib
-    .lookup<NativeFunction<Int32 Function(Int32, Int32)>>("native_add")
-    .asFunction();
+// final int Function(int x, int y) ouisync =
+// ouisyncLib
+//     .lookup<NativeFunction<Int32 Function(Int32, Int32)>>("native_add")
+//     .asFunction();
 
-final nRegisterPostCObject = nativeAddLib.lookupFunction<
+final nRegisterPostCObject = ouisyncLib.lookupFunction<
     Void Function(
         Pointer<NativeFunction<Int8 Function(Int64, Pointer<Dart_CObject>)>>
         functionPointer),
@@ -20,15 +20,15 @@ final nRegisterPostCObject = nativeAddLib.lookupFunction<
         Pointer<NativeFunction<Int8 Function(Int64, Pointer<Dart_CObject>)>>
         functionPointer)>('RegisterDart_PostCObject');
 
-final nRunTaskAsync = nativeAddLib
-    .lookupFunction<Int32 Function(Int64, Int8), int Function(int, int)>('native_add');
+// final nRunTaskAsync = ouisyncLib
+//     .lookupFunction<Int32 Function(Int64, Int8), int Function(int, int)>('native_add');
 
 class NativeCallbacks {
   static doSetup() {
     nRegisterPostCObject(NativeApi.postCObject);
   }
 
-  static Future<int> runAsyncTask(int taskId) async {
-    return singleResponseFuture((port) => nRunTaskAsync(port.nativePort, taskId));
-  }
+  // static Future<int> runAsyncTask(int taskId) async {
+  //   return singleResponseFuture((port) => nRunTaskAsync(port.nativePort, taskId));
+  // }
 }
