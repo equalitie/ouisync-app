ceebc1d299edb878f2c0c43ea24d93b7d5152a38
Jorge Pabon <jmpabon@gmail.com>
2021-01-27 16:00:31 +0800

    Implementation for reading the directory using the ouisync library restablished. Also: New macro ALOG (androidlog.h) used for logging

--- ios/Classes/localRepository.cpp
+++ ios/Classes/localRepository.cpp
@@ -12,22 +12,24 @@
 #include "options.h"
 #include "path.h"
 
+#include "androidlog.h"
+#define  LOG_TAG "LOCAL_REPOSITORY"
+
 #include <boost/asio.hpp>
 #include <boost/filesystem.hpp>
 
 #include <iostream>
+#include <iterator>
 
 using namespace std;
 using namespace ouisync;
 
 void readDir(Dart_Port callbackPort, string dir) {
-    fs::path p = dir;
-    for (auto f:p) {
-        cout << f;
-    }
-    /*net::io_context ioc;
+    net::io_context ioc;
 
     Options options;
+
+    ALOG(LOG_TAG, "dir=%s at %s:%s:%d", "directory", __FILE__,__FUNCTION__, __LINE__);
 
     string basedir = "/";
     vector<const char*> args = { "ouisync", "--basedir", basedir.c_str() };
@@ -49,9 +51,23 @@ void readDir(Dart_Port callbackPort, string dir) {
 
     Repository repo(ioc.get_executor(), options);
     Path path = fs::path(dir);
+    vector<string> files;
+
+    ALOG(LOG_TAG, "path=%s at %s:%s:%d", "path", __FILE__,__FUNCTION__, __LINE__);
 
     co_spawn(ioc, [&] () -> net::awaitable<void> {
-        vector<string> files = co_await repo.readdir(path_range(path));
-        callbackToDartStrArray(callbackPort, files);
-    }, net::detached);*/
+        files = co_await repo.readdir(path_range(path));
+
+    if (!files.empty()){
+        ostringstream vts;
+
+        copy(files.begin(), files.end()-1, ostream_iterator<string>(vts, ", "));
+        vts << files.back();
+
+        string files_string = vts.str();
+        ALOG(LOG_TAG, "files in %s at %s:%s:%d", "file contents", __FILE__,__FUNCTION__, __LINE__);
+    }
+
+    callbackToDartStrArray(callbackPort, files);
+    }, net::detached);
 }
