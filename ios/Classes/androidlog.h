28ab13191215130fab77f88416a71567ed2e9be3
Jorge Pabon <jmpabon@gmail.com>
2021-01-27 15:45:41 +0800

    Added: Header file for defining a macro for logging

---
+++ ios/Classes/androidlog.h
@@ -0,0 +1,11 @@
+//
+// Created by Jorge Pabon on 1/27/21.
+//
+
+#ifndef OUISYNC_APP_ANDROIDLOG_H
+#define OUISYNC_APP_ANDROIDLOG_H
+
+#include <android/log.h>
+#define  ALOG(...)  __android_log_print(ANDROID_LOG_INFO,__VA_ARGS__)
+
+#endif //OUISYNC_APP_ANDROIDLOG_H
