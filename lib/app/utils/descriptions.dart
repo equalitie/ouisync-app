4b9432605bd95b9151944ebfc80f3110edbf2f6b
Jorge Pabon <jmpabon@gmail.com>
2021-02-16 11:34:13 +0800

    Added: descriptions.dart => Util class for enum descriptions

---
+++ lib/app/utils/descriptions.dart
@@ -0,0 +1,28 @@
+import 'package:ouisync_app/app/controls/repo/repofooter.dart';
+
+class Descriptions {
+
+  static String getSyncStatusDescription(SyncStatus status){
+    String description;
+    switch(status){
+      case SyncStatus.syncing:
+        description = "syncing";
+        break;
+      case SyncStatus.idle:
+        description = "ok";
+        break;
+      case SyncStatus.paused:
+        description = "paused";
+        break;
+      case SyncStatus.stopped:
+        description = "stopped";
+        break;
+      case SyncStatus.problem:
+        description = "problem";
+        break;
+    }
+
+    return description;
+  }
+
+}
