2b54000b30317f8aa3fbf778c04102c42609b199
Jorge Pabon <jmpabon@gmail.com>
2021-02-24 19:44:30 +0800

    More dummy data added for UI placeholders Also: the return type of the native function readDirAsync is of type List<dynamic>

--- lib/app/data/repositories/directoryrepository.dart
+++ lib/app/data/repositories/directoryrepository.dart
@@ -1,38 +1,57 @@
-import 'package:ouisync_app/app/controls/repo/repofooter.dart';
-import 'package:ouisync_app/app/models/item/baseitem.dart';
-import 'package:ouisync_app/app/models/item/folderitem.dart';
+import 'package:ouisync_app/app/controls/controls.dart';
 import 'package:ouisync_app/app/models/models.dart';
-import 'package:ouisync_app/app/models/user/user.dart';
 import 'package:ouisync_app/callbacks/nativecallbacks.dart';
 
 class DirectoryRepository {
-  List<BaseItem> rootContents = [
-    FolderItem("1", "Folder 1", "/folder1", 123.45, SyncStatus.idle, const User(id: "A", name: "Me")),
-    FolderItem("2", "Folder 2", "/folder2", 789.01, SyncStatus.syncing, const User(id: "B", name: "Other")),
-    FileItem("a", "File 1.txt", "txt", "/", 23.67, SyncStatus.idle, const User(id: "A", name: "Me")),
-    FileItem("b", "File 2.txt", "txt", "/", 345.08, SyncStatus.idle, const User(id: "A", name: "Me")),
-    FileItem("c", "File 3.txt", "txt", "/", 45676.00, SyncStatus.idle, const User(id: "A", name: "Me")),
-    FileItem("d", "File 4.txt", "txt", "/", 32.79, SyncStatus.idle, const User(id: "A", name: "Me")),
-    FileItem("e", "File 5.txt", "txt", "/", 267.09, SyncStatus.idle, const User(id: "A", name: "Me")),
+  List<BaseItem> repos = [
+    FolderItem("0", "Repo XYZ", "/repo XYZ", 12.45, SyncStatus.idle, const User(id: "A", name: "Me")),
+  ];
+
+  List<BaseItem> repoXYZContents = [
+    FolderItem("1", "Folder 1", "/repo XYZ/folder 1", 123.45, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FolderItem("2", "Folder 2", "/repo XYZ/folder 2", 789.01, SyncStatus.syncing, const User(id: "B", name: "Other")),
+    FileItem("a", "File 1.txt", "txt", "/repo XYZ/File 1.txt", 23.67, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("b", "File 2.txt", "txt", "/repo XYZ/File 2.txt", 345.08, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("c", "File 3.txt", "txt", "/repo XYZ/File 3.txt", 45676.00, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("d", "File 4.txt", "txt", "/repo XYZ/File 4.txt", 32.79, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("e", "File 5.txt", "txt", "/repo XYZ/File 5.txt", 267.09, SyncStatus.idle, const User(id: "A", name: "Me")),
   ];
 
   List<BaseItem> folder1Contents = [
-    FileItem("a", "File 6.txt", "txt", "/", 23.67, SyncStatus.idle, const User(id: "A", name: "Me")),
-    FileItem("b", "File 7.txt", "txt", "/", 345.08, SyncStatus.idle, const User(id: "A", name: "Me")),
-    FileItem("c", "File 8.txt", "txt", "/", 45676.00, SyncStatus.idle, const User(id: "A", name: "Me")),
-    FileItem("d", "File 9.txt", "txt", "/", 32.79, SyncStatus.idle, const User(id: "A", name: "Me")),
-    FileItem("e", "File 10.txt", "txt", "/", 267.09, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FolderItem("1", "Folder 3", "/repo XYZ/folder 1/folder 3", 123.45, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FolderItem("2", "Folder 4", "/repo XYZ/folder 1/folder 4", 789.01, SyncStatus.syncing, const User(id: "B", name: "Other")),
+    FileItem("a", "File 6.txt", "txt", "/repo XYZ/folder 1/File 6.txt", 23.67, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("b", "File 7.txt", "txt", "/repo XYZ/folder 1/File 7.txt", 345.08, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("c", "File 8.txt", "txt", "/repo XYZ/folder 1/File 8.txt", 45676.00, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("d", "File 9.txt", "txt", "/repo XYZ/folder 1/File 9.txt", 32.79, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("e", "File 10.txt", "txt", "/repo XYZ/folder 1/File 10.txt", 267.09, SyncStatus.idle, const User(id: "A", name: "Me")),
+  ];
+
+  List<BaseItem> folder2Contents = [
+    FileItem("a", "File 11.txt", "txt", "/repo XYZ/folder 2/File 11.txt", 23.67, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("b", "File 12.txt", "txt", "/repo XYZ/folder 2/File 12.txt", 345.08, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("c", "File 13.txt", "txt", "/repo XYZ/folder 2/File 13.txt", 45676.00, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("d", "File 14.txt", "txt", "/repo XYZ/folder 2/File 14.txt", 32.79, SyncStatus.idle, const User(id: "A", name: "Me")),
+    FileItem("e", "File 15.txt", "txt", "/repo XYZ/folder 2/File 15.txt", 267.09, SyncStatus.idle, const User(id: "A", name: "Me")),
   ];
 
   Future<List<BaseItem>> getContents(String path) async {
-    List<String> files = await NativeCallbacks.readDirAsync(path);
+    print("About to call readDirAsync...");
+     List<dynamic> files = await NativeCallbacks.readDirAsync(path);
+    print("Files returned: " + files.toString());
 
     return Future
         .delayed(Duration(seconds: 2))
         .then((value) =>
     path == "/"
-        ? rootContents
-        : folder1Contents
+        ? repos
+        : path == "/repo XYZ"
+        ? repoXYZContents
+        : path == "/repo XYZ/folder 1"
+        ? folder1Contents
+        : path == "/repo XYZ/folder 2"
+        ? folder2Contents
+        : []
     );
   }
 }
