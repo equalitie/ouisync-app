8faefe179699949bdd21edf8c590bff568302a78
Jorge Pabon <jmpabon@gmail.com>
2021-03-04 18:29:44 +0800

    New callback to a the native function initializeOuisyncRepository added. Also: - Callback to native function readDir updated to include the new parameters => repo_dir & c_directory_to_read. - Small code reformatting .

--- lib/callbacks/nativecallbacks.dart
+++ lib/callbacks/nativecallbacks.dart
@@ -4,29 +4,64 @@ import 'dart:io';
 import 'package:isolate/ports.dart';
 import 'package:ffi/ffi.dart';
 
-final DynamicLibrary ouisyncLib = Platform.isAndroid
+final DynamicLibrary ouisyncLib = 
+Platform.isAndroid
     ? DynamicLibrary.open("libnativeapp.so")
     : DynamicLibrary.executable();
 
-final nRegisterPostCObject = ouisyncLib.lookupFunction<
+final nRegisterPostCObject = 
+  ouisyncLib.lookupFunction<
     Void Function(
-        Pointer<NativeFunction<Int8 Function(Int64, Pointer<Dart_CObject>)>>
-        functionPointer),
+      Pointer<NativeFunction<Int8 Function(
+        Int64, 
+        Pointer<Dart_CObject>
+      )>>functionPointer
+    ),
     void Function(
-        Pointer<NativeFunction<Int8 Function(Int64, Pointer<Dart_CObject>)>>
-        functionPointer)>('RegisterDart_PostCObject');
+      Pointer<NativeFunction<Int8 Function(
+        Int64, 
+        Pointer<Dart_CObject>
+      )>>functionPointer
+    )
+  >('RegisterDart_PostCObject');
 
-final nReadDirAsync = ouisyncLib
-    .lookupFunction<
-    Void Function(Int64, Pointer<Utf8>),
-    void Function(int, Pointer<Utf8>)>('readDir');
+final nInitializeOuisyncRepository = 
+  ouisyncLib.lookupFunction<
+    Void Function(
+      Int64,
+      Pointer<Utf8>
+    ),
+    void Function(
+      int,
+      Pointer<Utf8>
+    )
+  >('initializeOuisyncRepository');
+
+final nReadDirAsync = 
+  ouisyncLib.lookupFunction<
+    Void Function(
+      Int64, 
+      Pointer<Utf8>,
+      Pointer<Utf8>
+    ),
+    void Function(
+      int, 
+      Pointer<Utf8>,
+      Pointer<Utf8>
+    )
+  >('readDir');
 
 class NativeCallbacks {
   static doSetup() {
     nRegisterPostCObject(NativeApi.postCObject);
+    print('Native callbacks setup done');
   }
 
-  static Future<List<dynamic>> readDirAsync(String dir) async {
-    return singleResponseFuture((port) => nReadDirAsync(port.nativePort, Utf8.toUtf8(dir)));
+  static Future<dynamic> initializeOuisyncRepository(String repoDir) async {
+    return singleResponseFuture((port) => nInitializeOuisyncRepository(port.nativePort, Utf8.toUtf8(repoDir)));
+  } 
+
+  static Future<List<dynamic>> readDirAsync(String repoPath, String folderPath) async {
+    return singleResponseFuture((port) => nReadDirAsync(port.nativePort, Utf8.toUtf8(repoPath), Utf8.toUtf8(folderPath)));
   }
 }
