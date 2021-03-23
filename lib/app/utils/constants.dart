e186cd26e8b002aac81332f072b92cdcaa923da6
Jorge Pabon <jmpabon@gmail.com>
2021-03-23 13:15:32 +0800

    New constants added for pages title, various messages, and paddings.

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -1,8 +1,4 @@
-import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
-
-const appRootDirectory = "/data/user/0/com.equalitie.ouisync_app/files";
-const appDefaultFolder = "$appRootDirectory/branches";
 
 const List<PermissionStatus> negativePermissionStatus = [
     PermissionStatus.restricted,
@@ -10,3 +6,29 @@ const List<PermissionStatus> negativePermissionStatus = [
     PermissionStatus.denied,
     PermissionStatus.permanentlyDenied,
   ];
+
+const String repositoriesFolder = 'repos';
+
+const String titleApp = 'OuiSync';
+const String titleRootPage = 'Repositories';
+
+const String messageNoStoragePermissionsGranted = 'OuiSync need access to the phone storage to operate.'
+'\n\nWithout this permission the app can\'t function properly.'
+'\n\nTo grant the permissions needed, please follow the instructions below:\n'
+'\n   > Go to Settings'
+'\n   > Privacy'
+'\n   > Permission manager'
+'\n   > Storage'
+'\n   > Scroll down to the DENIED apps'
+'\n   > Look for OuiSync and select it'
+'\n   > Select Allow'
+'\n\nThen you can open OuiSync again and use it as expected';
+const String messageNoRepos = 'No repositories found';
+const String messageEmptyRepo = 'This repository is empty';
+const String messageEmptyFolder = 'This folder is empty';
+
+const String messageCreateNewRepoStyled = 'Create a new repo using <bold>Actions</bold> <arrow_down/>';
+const String messageCreateAddNewObjectStyled = 'Create a new folder, or add a file, using <bold>Actions</bold> <arrow_down/>';
+
+const double actionsDialogPadding = 20.0;
+const double actionsDialogAvatarRadius = 45.0;
