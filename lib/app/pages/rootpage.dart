48a993341024e64241247d6ac32d6eaa776ef944
Jorge Pabon <jmpabon@gmail.com>
2021-03-23 13:27:58 +0800

    Refactored: moved the repo initialization to the root page instead of the life cycle page to fix some issues with the navigation and the bloc patter implementation. Also: new floating action button called Actions, which open a custom dialog for creating a new repo (just that for now), and the bloc provider for the folders (DirectoryBloc) is now used here, when navigating to a folder (this way it is a bloc per folder, and the contents will not be replaced in the parent by the children.

--- lib/app/pages/rootpage.dart
+++ lib/app/pages/rootpage.dart
@@ -1,10 +1,33 @@
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:styled_text/styled_text.dart';
+
+import '../../callbacks/nativecallbacks.dart';
+import '../bloc/blocs.dart';
+import '../controls/controls.dart';
+import '../data/data.dart';
+import '../models/models.dart';
+import '../utils/utils.dart';
+import 'pages.dart';
 
 class RootPage extends StatefulWidget {
-  RootPage({Key key, this.title}) : super(key: key);
+  RootPage({
+    Key key,
+    @required this.reposBaseFolderPath,
+    @required this.foldersRepository,
+    this.title,
+  }) :
+  assert(reposBaseFolderPath != null),
+  assert(reposBaseFolderPath != ""), 
+  assert(foldersRepository != null),
+  super(key: key);
 
+  final String reposBaseFolderPath;
+  final DirectoryRepository foldersRepository;
   final String title;
 
   @override
@@ -12,28 +35,231 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  final _createRepoFormKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    initRepositories();
+
+    super.initState();
+  }
+
+  Future<void> initRepositories() async {
+    bool exist = await Directory(widget.reposBaseFolderPath).exists();
+    if (!exist) {
+      print('No repositories were found');
+      BlocProvider.of<RepositoryBloc>(context)
+      .add(
+        RepositoriesRequest(
+          repositoriesPath: widget.reposBaseFolderPath
+        )
+      );
+
+      return;
+    }
+
+    List<FileSystemEntity> repoList = await Directory(widget.reposBaseFolderPath).list().toList();
+    print('Repositories found ${repoList.length}:\n$repoList');
+
+    initializeUserRepositories(repoList.map((e) => e.path).toList())
+    .then((value) async => 
+      BlocProvider.of<RepositoryBloc>(context)
+      .add(
+        RepositoriesRequest(
+          repositoriesPath: widget.reposBaseFolderPath
+        )
+      )
+    );
+  }
+
+  Future<void> initializeUserRepositories(List<String> reposList) async {
+    reposList.forEach((repo) => {
+      print('About to initilialize $repo'),
+      NativeCallbacks.initializeOuisyncRepository(repo)
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget> [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+
+            },
+          )
+        ]
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              'OuiSync',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 48.0,
-                fontWeight: FontWeight.bold
-              ),),
-            ),
-        ],
+      drawer: Drawer(
+        child: Center(child: DrawerMenu()),
       ),
+      body: _repositoriesBlocBuilder(),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text('Actions'),
+        backgroundColor: Colors.blue,
+        onPressed: () {
+          return showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return ActionsDialog(
+                title: 'Actions',
+                body: _repositoriesActions(),
+              );
+            }
+          );
+        },
+      ),
+    );
+  }
+
+  _repositoriesActions() => Column(
+    children: [
+      _createRepository(),
+    ],
+  );
+
+  _createRepository() => Form(
+    key: _createRepoFormKey,
+    autovalidateMode: AutovalidateMode.onUserInteraction,
+    child: Column(
+      children: [
+        TextFormField(
+          decoration: InputDecoration (
+            icon: const Icon(Icons.folder),
+            hintText: 'Repository name',
+            labelText: 'Create a new repository',
+            contentPadding: EdgeInsets.all(10.0),
+          ),
+          validator: (value) {
+            return value.isEmpty
+            ? 'Please enter some text'
+            : null;
+          },
+          onSaved: (newRepoName) {
+            BlocProvider.of<RepositoryBloc>(context)
+            .add(
+              RepositoryCreate(
+                repoDir: widget.reposBaseFolderPath,
+                newRepoRelativePath: newRepoName
+              )
+            );
+
+            Navigator.of(context).pop();
+          },
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (_createRepoFormKey.currentState.validate()) {
+              _createRepoFormKey.currentState.save();
+            }
+          },
+          child: const Text('create'),
+        ),
+      ],
+    )
+  );
+
+  Widget _repositoriesBlocBuilder() {
+    return Center(
+        child: BlocBuilder<RepositoryBloc, RepositoryState>(
+            builder: (context, state) {
+              if (state is RepositoryInitial) {
+                return Center(child: Text('Loading repositories...'));
+              }
+
+              if (state is RepositoryLoadInProgress){
+                return Center(child: CircularProgressIndicator());
+              }
+
+              if (state is RepositoryLoadSuccess) {
+                return state.repositories.isEmpty
+                ? _noRepos()
+                : _reposListView(context, state.repositories);
+              }
+
+              if (state is RepositoryLoadFailure) {
+                return Text(
+                  'Something went wrong!',
+                  style: TextStyle(color: Colors.red),
+                );
+              }
+
+              return Center(child: Text('repos_'));
+            }
+        )
+    );
+  }
+
+  _noRepos() => Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      Align(
+        alignment: Alignment.center,
+        child: Text(
+          messageNoRepos,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+      SizedBox(height: 20.0),
+      Align(
+        alignment: Alignment.center,
+        child: StyledText(
+          text: messageCreateNewRepoStyled,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.normal
+          ),
+          styles: {
+            'bold': TextStyle(fontWeight: FontWeight.bold),
+            'arrow_down': IconStyle(Icons.south),
+          },
+        ),
+      ),
+    ],
+  );
+
+  _reposListView(BuildContext context, List<BaseItem> repositories) => ListView.builder(
+    itemCount: repositories.length,
+    itemBuilder: (context, index) {
+      final repo = repositories[index];
+      return RepoDescription(
+        folderData: repo,
+        isEncrypted: false,
+        isLocal: true,
+        isOwn: true,
+        action: () => _navigateToRepositoryContents(
+          context: context,
+          repoPath: repo.path,
+          title: repo.name
+        )
+      );
+    },
+  );
+
+  void _navigateToRepositoryContents({BuildContext context, String repoPath, String title}) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return BlocProvider(
+          create: (context) => DirectoryBloc(
+            repository: widget.foldersRepository
+          ),
+          child: FolderPage(
+            repoPath: repoPath,
+            folderPath: '',
+            foldersRepository: widget.foldersRepository,
+            title: title
+          )
+        );
+      })
     );
   }
 }
