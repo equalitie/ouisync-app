b281311c24c78105cc94e13b5531393252f8e309
Jorge Pabon <jmpabon@gmail.com>
2021-03-23 12:50:44 +0800

    onError method fixed after updating the bloc library to the latest version.

--- lib/app/bloc/simpleblocobserver.dart
+++ lib/app/bloc/simpleblocobserver.dart
@@ -14,8 +14,8 @@ class SimpleBlocObserver extends BlocObserver {
   }
 
   @override
-  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
     print("onError $error");
-    super.onError(cubit, error, stackTrace);
+    super.onError(bloc, error, stackTrace);
   }
 }
