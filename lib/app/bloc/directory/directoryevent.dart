00e50519fadaf00077234333ba074919e7df346c
Jorge Pabon <jmpabon@gmail.com>
2021-05-11 14:17:04 +0800

    New events added to the directory bloc
    - Create file
    - Read file
    
    Also: ContentRequest event renamed as RequestContent

--- lib/app/bloc/directory/directoryevent.dart
+++ lib/app/bloc/directory/directoryevent.dart
@@ -5,8 +5,8 @@ abstract class DirectoryEvent extends Equatable {
   const DirectoryEvent();
 }
 
-class FolderCreate extends DirectoryEvent {
-  const FolderCreate({
+class CreateFolder extends DirectoryEvent {
+  const CreateFolder({
     @required this.repoPath,
     @required this.parentPath,
     @required this.newFolderRelativePath
@@ -29,8 +29,8 @@ class FolderCreate extends DirectoryEvent {
   ];
 }
 
-class ContentRequest extends DirectoryEvent {
-  const ContentRequest({
+class RequestContent extends DirectoryEvent {
+  const RequestContent({
     @required this.repoPath,
     @required this.folderRelativePath,
   }) : 
@@ -44,6 +44,65 @@ class ContentRequest extends DirectoryEvent {
   List<Object> get props => [
     repoPath,
     folderRelativePath,
+  ];
+
+}
+
+class CreateFile extends DirectoryEvent {
+  const CreateFile({
+    @required this.repoPath,
+    @required this.parentPath,
+    @required this.newFileRelativePath,
+    @required this.fileStream
+  }) :
+  assert (repoPath != null),
+  assert (repoPath != ''),
+  assert (parentPath != null),
+  assert (newFileRelativePath != null),
+  assert (newFileRelativePath != ''),
+  assert (fileStream != null);
+
+  final String repoPath;
+  final String newFileRelativePath;
+  final String parentPath;
+  final Stream<List<int>> fileStream;
+
+  @override
+  List<Object> get props => [
+    repoPath,
+    parentPath,
+    newFileRelativePath,
+    fileStream
+  ];
+  
+}
+
+class ReadFile extends DirectoryEvent {
+  const ReadFile({
+    @required this.repoPath,
+    @required this.parentPath,
+    @required this.fileRelativePath,
+    @required this.totalBytes
+  }) :
+  assert (repoPath != null),
+  assert (repoPath != ''),
+  assert (parentPath != null),
+  assert (fileRelativePath != null),
+  assert (fileRelativePath != ''),
+  assert (totalBytes != null),
+  assert (totalBytes > 0);
+
+  final String repoPath;
+  final String parentPath;
+  final String fileRelativePath;
+  final double totalBytes;
+
+  @override
+  List<Object> get props => [
+    repoPath,
+    parentPath,
+    fileRelativePath,
+    totalBytes
   ];
 
 }
