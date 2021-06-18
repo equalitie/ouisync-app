c5d9a1c2a108ac2a91877febd5e65c6f5200bd7a
Jorge Pabon <jmpabon@gmail.com>
2021-06-18 13:35:48 -0400

    Bloc well file updated with the new files for the navigation bloc.

--- lib/app/bloc/blocs.dart
+++ lib/app/bloc/blocs.dart
@@ -1,6 +1,9 @@
 export 'directory/directorybloc.dart';
 export 'directory/directoryevent.dart';
 export 'directory/directorystate.dart';
+export 'navigation/nav_bloc.dart';
+export 'navigation/nav_event.dart';
+export 'navigation/nav_state.dart';
 export 'repository/repositorybloc.dart';
 export 'repository/repositoryevent.dart';
 export 'repository/repositorystate.dart';
