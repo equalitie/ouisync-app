d26463f3edae687ef172637fced836d9f962a5bf
Jorge Pabon <jmpabon@gmail.com>
2021-06-30 17:50:43 -0500

    The class NavigationState was modified to be a abstract class that is extended by NavigationLoadSuccess (an others).
    Also: enum Navigation (folder, file, receive_intent) added and used in the state.

--- lib/app/bloc/navigation/nav_event.dart
+++ lib/app/bloc/navigation/nav_event.dart
@@ -1,22 +1,27 @@
 import 'package:equatable/equatable.dart';
 
+import '../blocs.dart';
+
 abstract class NavigationEvent extends Equatable {
   const NavigationEvent();
 }
 
 class NavigateTo extends NavigationEvent {
   const NavigateTo(
+    this.navigation,
     this.origin,
     this.destination
   ) : 
   assert (origin != ''),
   assert (destination != '');
 
+  final Navigation navigation;
   final String origin;
   final String destination;
 
   @override
   List<Object?> get props => [
+    navigation,
     origin,
     destination
   ];
--- lib/app/bloc/navigation/nav_state.dart
+++ lib/app/bloc/navigation/nav_state.dart
@@ -1,19 +1,39 @@
 import 'package:equatable/equatable.dart';
 
-class NavigationState extends Equatable {
-  const NavigationState(
-    this.parentPath,
-    this.destinationPath
-  ) :
+abstract class NavigationState extends Equatable {
+  const NavigationState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class NavigationInitial extends NavigationState {}
+
+class NavigationLoadInProgress extends NavigationState {}
+
+class NavigationLoadSuccess extends NavigationState {
+  const NavigationLoadSuccess({
+    required this.navigation,
+    required this.parentPath,
+    required this.destinationPath
+  }) :
   assert (parentPath != ''),
   assert (destinationPath != '');
 
+  final Navigation navigation;
   final String parentPath;
   final String destinationPath;
 
   @override
   List<Object> get props => [
+    navigation,
     parentPath,
     destinationPath
   ];
+}
+
+enum Navigation {
+  folder,
+  file,
+  receive_intent,
 }
