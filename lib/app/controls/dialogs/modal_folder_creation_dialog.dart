20649fa658163f7641a2c95a5eef363ff6e7adaa
Jorge Pabon <jmpabon@gmail.com>
2021-08-25 16:00:57 +0800

    Added: modal_folder_creation_dialog.dart replacing the file AddFolderPage.dart

---
+++ lib/app/controls/dialogs/modal_folder_creation_dialog.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ouisync_plugin/ouisync_plugin.dart';
+
+import '../../bloc/blocs.dart';
+import '../../utils/utils.dart';
+
+class FolderCreation extends StatelessWidget {
+  FolderCreation({
+    Key? key,
+    required this.session,
+    required this.bloc,
+    required this.path,
+    required this.formKey
+  }) : super(key: key);
+
+  final Session session;
+  final Bloc bloc;
+  final String path;
+  final GlobalKey<FormState> formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Form(
+      key: this.formKey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Container(
+        margin: const EdgeInsets.all(16.0),
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: theme.cardColor,
+          borderRadius: const BorderRadius.all(Radius.circular(16.0))
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _buildCreateFolderWidget(context),
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget _buildCreateFolderWidget(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          buildEntry(context, 'Create a new folder: ', (value) => _onSaved(context, value)),
+          buildInfoLabel('Location: ', this.path),
+          buildActionsSection(context, _actions(context)),
+        ]
+      )
+    );
+  }
+
+  void _onSaved(context, newFolderName) {
+    final newFolderPath = this.path == slash
+    ? '/$newFolderName'
+    : '${this.path}/$newFolderName';  
+
+    this.bloc
+    .add(
+      CreateFolder(
+        session: this.session,
+        parentPath: this.path,
+        newFolderPath: newFolderPath
+      )
+    );
+
+    Navigator.of(context).pop(true);
+  }
+
+  List<Widget> _actions(context) => [
+    ElevatedButton(
+      onPressed: () {
+        if (this.formKey.currentState!.validate()) {
+            this.formKey.currentState!.save();
+          }
+      },
+      child: Text('Create')
+    ),
+    SizedBox(width: 20.0,),
+    OutlinedButton(
+      onPressed: () => Navigator.of(context).pop(false),
+      child: Text('Cancel')
+    ),
+  ];
+}
