08dcbc53b86cc1f7584d5160dd1bbc1439b93ecd
Jorge Pabon <jmpabon@gmail.com>
2021-09-07 11:09:26 +0800

    Unused import removed

--- lib/app/pages/root_ouisync.dart
+++ lib/app/pages/root_ouisync.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ouisync_plugin/ouisync_plugin.dart';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
