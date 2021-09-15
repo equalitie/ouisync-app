520c6f0a9726c2cfb59fb64b1463175e9cc7f994
Jorge Pabon <jmpabon@gmail.com>
2021-09-15 17:21:26 +0800

    Small clean up

--- lib/app/controls/controls.dart
+++ lib/app/controls/controls.dart
@@ -1,4 +1,5 @@
 export 'dialogs/actionsdialog.dart';
+
 export 'dialogs/modal_file_detail_bottom_sheet.dart';
 export 'dialogs/modal_folder_creation_dialog.dart';
 export 'dialogs/modal_folder_detail_bottom_sheet.dart';
