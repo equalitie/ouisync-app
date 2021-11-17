2a0f14d80a22ff96a2c1eff2237937c3af0462a0
Jorge Pabon <jmpabon@gmail.com>
2021-11-17 13:52:21 +0800

    Route BLoC test updated
    Unnecessaries fake, fallback registration and dummy data removed.

--- test/route_bloc_test.dart
+++ test/route_bloc_test.dart
@@ -1,31 +1,8 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:equatable/equatable.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:ouisync_app/app/bloc/blocs.dart';
-import 'package:ouisync_app/app/models/models.dart';
 
-class UpdateRouteStateFake 
-  extends Fake
-  implements UpdateRoute, Equatable {
-    @override
-    List<Object> get props => [
-      path,
-      action
-    ];}
-
-late FolderItem dummyFolderItem;
 void main() {
-  setUpAll(() {
-    registerFallbackValue<RouteEvent>(UpdateRouteStateFake());
-
-    dummyFolderItem = FolderItem(
-      name: 'test',
-      path: '/test',
-      items: <BaseItem>[]
-    );
-  });
   // TODO: Expand this test suite
   blocTest('Emits a list of widgets representing the path',
     build: () => RouteBloc(),
