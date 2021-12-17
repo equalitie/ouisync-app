06914792a97c4e777ef8741a05bf2fb28cae8488
Jorge Pabon <jmpabon@gmail.com>
2021-12-17 18:26:06 +0800

    Added util class InlineTextStyles for formatting strings using the styled_text plugin

---
+++ lib/app/utils/inline_text_styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:styled_text/styled_text.dart';
+
+class InlineTextStyles {
+  InlineTextStyles._();
+
+  static StyledTextTagBase bold = StyledTextTag(style: const TextStyle(fontWeight: FontWeight.bold));
+
+  static StyledTextTagBase size({double size = 28.0}) => StyledTextTag(style: TextStyle(fontSize: size));
+
+  static StyledTextTagBase color(Color color) => StyledTextTag(style: TextStyle(color: color));
+  
+  static StyledTextTagBase icon(IconData icon, {double? size, Color? color}) => StyledTextIconTag(
+    icon,
+    size: size,
+    color: color
+  );
+}
--- lib/app/utils/utils.dart
+++ lib/app/utils/utils.dart
@@ -7,6 +7,7 @@ export 'dialogs.dart';
 export 'entry_info.dart';
 export 'fields.dart';
 export 'format.dart';
+export 'inline_text_styles.dart';
 export 'settings.dart';
 export 'spinning_icon.dart';
 export 'strings.dart';
