557a0af79fbb8dbf671b2becc46cdb31b2481e7a
Jorge Pabon <jmpabon@gmail.com>
2021-12-17 18:29:30 +0800

    imports cleanup

--- lib/app/bloc/route/route_bloc.dart
+++ lib/app/bloc/route/route_bloc.dart
@@ -3,10 +3,8 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../utils/actions.dart';
 import '../../utils/utils.dart';
 
 part 'route_event.dart';
--- lib/app/custom_widgets/dialogs/modal_folder_creation_dialog.dart
+++ lib/app/custom_widgets/dialogs/modal_folder_creation_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:ouisync_plugin/ouisync_plugin.dart';
 
 import '../../bloc/blocs.dart';
