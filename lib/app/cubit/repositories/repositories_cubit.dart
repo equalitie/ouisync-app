68f448f3bf2e18dc927cfe922ea1127fe1304a25
Jorge Pabon <jmpabon@gmail.com>
2022-02-17 20:00:08 +0800

    Rename (Closes #39) and delete (Closes #38) repositories in the settings page.
    - RepositoryHelper extended to include functions for rename and delete the `*.db` files of repositories.
    - Repository Cubit now includes functions for rename and delete a repository.
    - RepositoriesService now uses PersistedRepository to keep repositories in memory.
    - RepositoriesService now handles the callback for the notification of the current repository.
    - Global member for the current repository and subscription to its notifications, moved into RepositoriesService from the main page.

--- lib/app/cubit/repositories/repositories_cubit.dart
+++ lib/app/cubit/repositories/repositories_cubit.dart
@@ -2,8 +2,10 @@ import 'dart:io' as io;
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:ouisync_app/app/utils/utils.dart';
 import 'package:ouisync_plugin/ouisync_plugin.dart';
+
+import '../../services/services.dart';
+import '../../utils/utils.dart';
 
 part 'repositories_state.dart';
 
@@ -58,28 +60,69 @@ class RepositoriesCubit extends Cubit<RepositoryPickerState> {
       ));
   }
 
-  void renameRepository({
-    Repository? repository,
-    required String oldName,
-    required String newName
-  }) async {
+  /// Renames a repository
+  /// 
+  /// 1. Remove the repiository from memory.
+  /// 2. Reset the default repository setting.
+  /// 3. Rename the *.db files in the local storage.
+  /// 4. Get the new default repository from the remaining repositories, if any.
+  /// 5. Get the default repository object from memory, if any.
+  /// 6. Emits the event for selecting a new repository: this updates the
+  ///    repository picker, and from there, the state in the main page. 
+  void renameRepository(String oldName, String newName) async {
+    final repositoriesService = RepositoriesService();
+    repositoriesService.remove(newName); // 1
+
+    await Settings.saveSetting(Constants.currentRepositoryKey, ''); // 2
+
+    RepositoryHelper.renameRepositoryFiles(repositoriesDir, 
+      oldName: oldName,
+      newName: newName
+    ); // 3
+
+    final latestRepositoryOrDefaultName = await RepositoryHelper
+    .latestRepositoryOrDefault(null); //4
+    
+    final newDefaultRepository = repositoriesService
+    .get(latestRepositoryOrDefaultName); // 5
+
     emit(RepositoryPickerSelection(
-      repository: null,
-      name: ''
-    ));
-
-    final repositoriesDir = await Settings.readSetting(Constants.repositoriesDirKey);
-    final files = await io.Directory(repositoriesDir).list().where((element) => element.path.contains(oldName)).toList();
-    files.forEach((element) { element.renameSync('$repositoriesDir/$newName.db'); });
-
-    print(io.Directory(repositoriesDir).listSync());
-
-    emit(RepositoryPickerSelection(
-      repository: null,
-      name: newName
-    ));
+      repository: newDefaultRepository!.repository,
+      name: newDefaultRepository.name
+    )); // 6
   }
 
+  /// Deletes a repository
+  /// 
+  /// 1. Remove the repiository from memory.
+  /// 2. Reset the default repository setting.
+  /// 3. Deletes the *.db files from the local storage
+  /// 4. Get the new default repository from the remaining repositories, if any.
+  /// 5. Get the default repository object from memory, if any.
+  /// 6. Emits the event for selecting a new repository: this updates the
+  ///    repository picker, and from there, the state in the main page. 
+  void deleteRepository(String repositoryName) async {
+    final repositoriesService = RepositoriesService();
+    repositoriesService.remove(repositoryName); // 1
+
+    await Settings.saveSetting(Constants.currentRepositoryKey, ''); // 2
+
+    RepositoryHelper.deleteRepositoryFiles(
+      repositoriesDir,
+      repositoryName: repositoryName
+    ); // 3
+
+    final latestRepositoryOrDefaultName = await RepositoryHelper
+    .latestRepositoryOrDefault(null); // 4
+    
+    final newDefaultRepository = repositoriesService
+    .get(latestRepositoryOrDefaultName); // 5
+
+    emit(RepositoryPickerSelection(
+      repository: newDefaultRepository!.repository,
+      name: newDefaultRepository.name
+    )); // 6
+  }
 
   _buildStoreString(repositoryName) => '${this.repositoriesDir}/$repositoryName.db';
 
