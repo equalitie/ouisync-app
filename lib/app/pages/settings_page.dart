fa95667ac4f6e7bdda9e460f258a0ce66fc63629
Jorge Pabon <jmpabon@gmail.com>
2022-02-18 22:27:43 +0800

    Settings page updated
    - Text buttons replaced with icon buttons.
    - Share button added
    - Confirmation dialog when deleting a repository added.
    - RepositoryPicker replaced with custom dropdown, to follow the wireframe

--- lib/app/pages/settings_page.dart
+++ lib/app/pages/settings_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ouisync_app/app/custom_widgets/dialogs/modal_rename_repository_dialog.dart';
+import 'package:ouisync_app/app/models/data_models/persisted_repository.dart';
 
 import '../cubit/cubits.dart';
 import '../custom_widgets/custom_widgets.dart';
@@ -27,6 +29,8 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   RepositoriesService _repositoriesSession = RepositoriesService();
+
+  PersistedRepository? _persistedRepository;
   bool _bittorrentDhtStatus = false;
 
   @override
@@ -35,6 +39,10 @@ class _SettingsPageState extends State<SettingsPage> {
 
     _bittorrentDhtStatus = widget.dhtStatus;
     print('BitTorrent DHT status: ${widget.dhtStatus}');
+
+    setState(() {
+      _persistedRepository = _repositoriesSession.current;
+    });
   }
 
   @override
@@ -60,7 +68,7 @@ class _SettingsPageState extends State<SettingsPage> {
               padding: const EdgeInsets.all(0.0),
               value: _bittorrentDhtStatus,
               onChanged: updateDhtSetting,
-            )
+            ),
           ],
         )
       )
@@ -72,54 +80,165 @@ class _SettingsPageState extends State<SettingsPage> {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Fields.iconLabel(
-            icon: Icons.lock_rounded,
-            text: Strings.titleRepository,
-            iconSize: Dimensions.sizeIconBig,
-            textAlign: TextAlign.start,
+          Fields.idLabel(Strings.titleRepository,
+            fontSize: Dimensions.fontAverage,
+            fontWeight: FontWeight.normal
+          ),
+          Dimensions.spacingVertical,
+          Container(
+            decoration: BoxDecoration(
+              borderRadius : BorderRadius.only(
+                topLeft: Radius.circular(4),
+                topRight: Radius.circular(4),
+                bottomLeft: Radius.circular(0),
+                bottomRight: Radius.circular(0),
+              ),
+              color : Color.fromRGBO(33, 33, 33, 0.07999999821186066),
             ),
-          RepositoryPicker(
-            repositoriesCubit: widget.repositoriesCubit,
-            onRepositorySelect: widget.onRepositorySelect,
-            borderColor: Colors.black38,
-            currentRepository: _repositoriesSession.current?.repository,
-            currentRepositoryName: _repositoriesSession.current?.name ?? '',
-          ),
-          SizedBox(height: 20.0,),
-          TextButton(
-            onPressed: () {
-              if (!_repositoriesSession.hasCurrent) {
-                return;
-              }
-              
-              widget.repositoriesCubit
-              .renameRepository(_repositoriesSession.current!.name, 'test');
-            },
-            child: Text(
-              Strings.actionEditRepositoryName,
-              style: TextStyle(
-                fontSize: Dimensions.fontAverage
+            child: Container(
+              padding: Dimensions.paddingActionBox,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(Dimensions.radiusMicro)),
+                border: Border.all(
+                  color: Colors.black45,
+                  width: 1.0,
+                  style: BorderStyle.solid
+                ),
+                color: Colors.grey.shade300,
               ),
-            )
+              child: DropdownButton(
+                isExpanded: true,
+                value: _persistedRepository,
+                underline: SizedBox(),
+                items: _repositoriesSession.repositories.map((PersistedRepository persisted) {
+                  return DropdownMenuItem(
+                    value: persisted,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Fields.idLabel(
+                          Strings.labelSelectRepository,
+                          textAlign: TextAlign.start,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey.shade600
+                        ),
+                        Dimensions.spacingVerticalHalf,
+                        Row(
+                          children: [
+                            Fields.constrainedText(
+                              persisted.name,
+                              fontWeight: FontWeight.normal
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+                onChanged: (persisted) async {
+                  print('Selected: $persisted');
+                  
+                  setState(() {
+                    _persistedRepository = persisted as PersistedRepository;
+                  });
+                },
+              ),
+            ),
           ),
-          TextButton(
-            onPressed: () { 
-              if (!_repositoriesSession.hasCurrent) {
-                return;
-              }  
+          Dimensions.spacingVertical,
+          Row(
+            children: [
+              Expanded(
+                child: Fields.actionText(
+                  Strings.actionEdit,
+                  textFontSize: Dimensions.fontAverage,
+                  icon: Icons.edit,
+                  iconSize: Dimensions.sizeIconSmall,
+                  spacing: Dimensions.spacingHorizontalHalf,
+                  onTap: () async {
+                    if (!_repositoriesSession.hasCurrent) {
+                      return;
+                    }
 
-              widget.repositoriesCubit
-              .deleteRepository(_repositoriesSession.current?.name ?? '');
-            },
-            child: Text(
-              Strings.actionDeleteRepository,
-              style: TextStyle(
-                fontSize: Dimensions.fontAverage
+                    await showDialog<String>(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (BuildContext context) {
+                        final formKey = GlobalKey<FormState>();
+
+                        return ActionsDialog(
+                          title: Strings.messageRenameRepository,
+                          body: RenameRepository(
+                            context: context,
+                            formKey: formKey,
+                            repositoryName: _repositoriesSession.current!.name
+                          ),
+                        );
+                      }
+                    ).then((newName) {
+                      if (newName?.isNotEmpty ?? false) {
+                        widget.repositoriesCubit
+                        .renameRepository(_repositoriesSession.current!.name, newName!);
+                      }
+                    });
+                  }
+                ),
               ),
-            )
+              Expanded(
+                child: Fields.actionText(
+                  Strings.actionShare,
+                  textFontSize: Dimensions.fontAverage,
+                  icon: Icons.share,
+                  iconSize: Dimensions.sizeIconSmall,
+                  spacing: Dimensions.spacingHorizontalHalf,
+                  onTap: widget.onShareRepository
+                )
+              ),
+              Expanded(
+                child: Fields.actionText(
+                  Strings.actionDelete,
+                  textFontSize: Dimensions.fontAverage,
+                  textColor: Colors.red,
+                  icon: Icons.delete,
+                  iconSize: Dimensions.sizeIconSmall,
+                  iconColor: Colors.red,
+                  spacing: Dimensions.spacingHorizontalHalf,
+                  onTap: () {
+                    if (!_repositoriesSession.hasCurrent) {
+                      return;
+                    }
+
+                    final actions = [
+                      TextButton(
+                        child: const Text(Strings.actionDeleteCapital),
+                        onPressed: () => 
+                        Navigator.of(context).pop(true),
+                      ),
+                      TextButton(
+                        child: const Text(Strings.actionCloseCapital),
+                        onPressed: () => 
+                        Navigator.of(context).pop(false),
+                      )
+                    ];
+
+                    Dialogs.simpleAlertDialog(
+                      context: context,
+                      title: Strings.titleDeleteRepository,
+                      message: Strings.messageConfirmRepositoryDeletion,
+                      actions: actions
+                    ).then((delete) {
+                      if (delete ?? false) {
+                        widget.repositoriesCubit
+                        .deleteRepository(_repositoriesSession.current?.name ?? '');
+                      }
+                    });
+                  }
+                )
+              )
+            ],
           ),
         ]
-      
     );
   }
 
