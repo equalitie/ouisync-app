0a7ce69c7c86b685dc8018cdf0ecfd9679cbf0e5
Peter Jankuliak <p.jankuliak@gmail.com>
2022-02-21 10:10:51 +0100

    Update the signing utility to take build number as argument

--- util/build-signed-bundle.sh
+++ util/build-signed-bundle.sh
@@ -3,8 +3,9 @@
 set -e
 
 function usage {
-    echo "Usage: cd <ouisync-app> && ./util/$(basename $0) <build-name> <keystore> <storepass>"
+    echo "Usage: cd <ouisync-app> && ./util/$(basename $0) <build-name> <build-number> <keystore> <storepass>"
     echo "    <build-name>    a version triple such as 0.0.1 or 0.0.1-rc1"
+    echo "    <build-number>  the build number (use 'flutter build appbundle -h' for more info)"
     echo "    <keystore>      path to the keystore (.jks) file"
     echo "    <storepass>     path to the containing the password for unlocking the keystore"
 }
@@ -21,8 +22,14 @@ if [ ! -f ./pubspec.yaml ]; then
 fi
 
 build_name="$1" #e.g. "0.0.1-rc1"
-keystore="$2"
-storepass="$3"
+build_number="$2"
+keystore="$3"
+storepass="$4"
+
+if [ -z "$build_number" ]; then
+    echo "The build number argument can't be an empty string"
+    exit 1
+fi
 
 if [ ! -f "$keystore" ]; then
     echo "No such <keystore> file '$keystore'"
@@ -34,6 +41,7 @@ if [ ! -f "$storepass" ]; then
     exit 1
 fi
 
+dirty=""
 if [[ $(git diff --stat) != '' ]]; then
     while true; do
         echo "Git is dirty:"
@@ -41,6 +49,7 @@ if [[ $(git diff --stat) != '' ]]; then
         read answer
         case "$answer" in
             y)
+                dirty="-dirty"
                 break
                 ;;
             d)
@@ -62,11 +71,16 @@ date_tag=$(date -u "+%Y-%m-%d--%H-%M-%S--UTC")
 git_commit="$(git rev-parse --short HEAD)"
 input_bundle=./build/app/outputs/bundle/release/app-release.aab
 
-NO_SIGN="true" flutter build appbundle --release --build-name=$build_name-$git_commit
+NO_SIGN="true" \
+    flutter build appbundle \
+    --release \
+    --build-number=$build_number \
+    --build-name=$build_name-$git_commit
+
 jarsigner -keystore "$keystore" -storepass:file $storepass $input_bundle upload
 
 mkdir -p ./build/releases/
 
-output_bundle=./build/releases/ouisync-release--v${build_name}--${date_tag}--$git_commit.aab
+output_bundle=./build/releases/ouisync-release--v${build_name}--$build_number--${date_tag}--${git_commit}${dirty}.aab
 echo "Moving signed bundle to $output_bundle"
 mv $input_bundle $output_bundle
