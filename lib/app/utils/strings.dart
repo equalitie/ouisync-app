f695710b442211afe70bca5ab9de3f2c288b2c8c
Jorge Pabon <jmpabon@gmail.com>
2022-02-23 00:40:40 +0800

    Some strings removed, updated.

--- lib/app/utils/strings.dart
+++ lib/app/utils/strings.dart
@@ -101,10 +101,7 @@ class Strings {
   static const String labelBitTorrentDHT = 'BitTorrent DHT';
   static const String labelSelectRepository = 'select repository';
   static const String labelRenameRepository = 'Enter the new name';
-  static const String labelProtocol = 'Protocol: ';
-  static const String labelIpAddress = 'IP address: ';
-  static const String labelPortNumber = 'Port number:';
-  static const String labelWiFiStatus = 'WiFi status: ';
+  static const String labelLocalEndpoint = 'Local endpoint: ';
   
   static const String messageMovingEntry = 'This function is not availabe when moving an entry';
   static const String messageRepositoryToken = 'Paste the token here';
@@ -155,7 +152,7 @@ class Strings {
   static const String replacementAccess = '[access]';
 
   static const String statusSync = 'SYNCED';
-  static const String statusUnspecified = '(unspecificed)';
+  static const String statusUnspecified = '(unspecified)';
 
   static const String iconInformation = 'Information';
   static const String iconPreview = 'Preview';
