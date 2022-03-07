b3ef122687deb0d53710ae1e7972d6b776c9bc6d
Jorge Pabon <jmpabon@gmail.com>
2022-03-07 17:58:42 +0800

    Added: error_state.dart
    The function in charge of building the error state was extracted as a StatelessWidget

---
+++ lib/app/custom_widgets/states/error_state.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:ouisync_app/app/utils/utils.dart';
+
+class ErrorState extends StatelessWidget {
+  const ErrorState({
+    Key? key,
+    required this.message,
+    required this.onReload
+  }) : super(key: key);
+
+  final String message;
+  final void Function()? onReload;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        reverse: false,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Fields.inPageMainMessage(
+                Strings.messageOhOh,
+                color: Colors.red,
+                tags: {
+                  Constants.inlineTextColor: InlineTextStyles.color(Colors.black),
+                  Constants.inlineTextSize: InlineTextStyles.size(),
+                  Constants.inlineTextBold: InlineTextStyles.bold
+                }
+              )
+            ),
+            SizedBox(height: 10.0),
+            Align(
+              alignment: Alignment.center,
+              child: Fields.inPageSecondaryMessage(
+                this.message,
+                tags: {
+                  Constants.inlineTextSize: InlineTextStyles.size(),
+                  Constants.inlineTextBold: InlineTextStyles.bold,
+                  Constants.inlineTextIcon: InlineTextStyles.icon(Icons.south)
+                }
+              )
+            ),
+            Dimensions.spacingVerticalDouble,
+            Fields.inPageButton(
+              onPressed: this.onReload,
+              text: Strings.actionReloadContents,
+              autofocus: true
+            )
+          ],
+        )
+      )
+    );
+  }
+}
