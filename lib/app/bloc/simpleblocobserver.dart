3a3eb278c1de3d7b6d90ca4b2c2b61cef280f7b1
Jorge Pabon <jmpabon@gmail.com>
2022-03-09 15:52:07 +0800

    Bloc observer migrated to 8.x format

--- lib/app/bloc/simpleblocobserver.dart
+++ lib/app/bloc/simpleblocobserver.dart
@@ -2,20 +2,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimpleBlocObserver extends BlocObserver {
   @override
-  void onEvent(Bloc bloc, Object? event) {
-    print("onEvent $event");
-    super.onEvent(bloc, event);
-  }
-
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    print("onTransition $transition");
-    super.onTransition(bloc, transition);
-  }
-
-  @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    print("onError $error");
-    super.onError(bloc, error, stackTrace);
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
   }
 }
