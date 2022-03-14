a7b1515d7de8c9c6d663865e1b912d015e5c5fe0
Jorge Pabon <jmpabon@gmail.com>
2022-03-14 15:57:26 +0800

    localizationsDelegates and supportedLocales setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:ouisync_plugin/ouisync_plugin.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'app/app.dart';
 import 'app/bloc/simpleblocobserver.dart';
 import 'app/utils/utils.dart';
+import 'generated/l10n.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,6 +34,13 @@ Future<void> main() async {
   BlocOverrides.runZoned(
     () => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       home: OuiSyncApp(
         session: session,
         appStorageLocation: appDir,
