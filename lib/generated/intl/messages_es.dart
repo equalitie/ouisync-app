92e63d5f1dfa5479066b72f20b7a35dae7c250f4
Jorge Pabon <jmpabon@gmail.com>
2022-03-14 16:15:25 +0800

    Localization clasess autogenerated after removing test string

--- lib/generated/intl/messages_es.dart
+++ lib/generated/intl/messages_es.dart
@@ -22,7 +22,6 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "appTitle": MessageLookupByLibrary.simpleMessage("OuiSync"),
-        "test": MessageLookupByLibrary.simpleMessage("Prueba")
+        "appTitle": MessageLookupByLibrary.simpleMessage("OuiSync")
       };
 }
