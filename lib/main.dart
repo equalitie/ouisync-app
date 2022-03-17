a7c5f45373db7bf51432a9db976866fa6986ab4c
Jorge Pabon <jmpabon@gmail.com>
2022-03-17 19:10:52 +0800

    Log printer selected by compilation mode
    PrettyDeveloperPrinter doesn't print logs in release mode,
    so the PrettyPrinter is used.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_loggy/flutter_loggy.dart';
+import 'package:loggy/loggy.dart';
 import 'package:ouisync_plugin/ouisync_plugin.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -11,6 +14,10 @@ import 'generated/l10n.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  Loggy.initLoggy(
+    logPrinter: kDebugMode ? const PrettyDeveloperPrinter() : const PrettyPrinter(),
+  );
 
   final appDir = (await getApplicationSupportDirectory()).path;
   final repositoriesDir = '$appDir/${Constants.folderRepositoriesName}';
