60c8449eb11dc00243e00b2666e8e98e820a37bc
Jorge Pabon <jmpabon@gmail.com>
2022-03-18 13:25:03 +0800

    Cutoms logger tag updated + new Loggy class extension to use a custom log level

--- lib/app/utils/loggers/ouisync_app_logger.dart
+++ lib/app/utils/loggers/ouisync_app_logger.dart
@@ -2,5 +2,11 @@ import 'package:loggy/loggy.dart';
 
 class OuiSyncAppLogger implements LoggyType {
   @override
-  Loggy<LoggyType> get loggy => Loggy<OuiSyncAppLogger>('ouisync-app - $runtimeType');
+  Loggy<LoggyType> get loggy => Loggy<OuiSyncAppLogger>('[$runtimeType]');
+}
+
+const LogLevel appLevel = LogLevel('ouisync-app', 1);
+
+extension OuiSyncAppLoggy on Loggy {
+  void app(dynamic message, Object? error, StackTrace? stackTrace) => log(appLevel, message, error, stackTrace);
 }
