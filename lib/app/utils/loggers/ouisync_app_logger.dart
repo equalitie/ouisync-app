fab59c993995c495f8c8f26aad62392313c2ff67
Jorge Pabon <jmpabon@gmail.com>
2022-03-21 15:27:45 +0800

    'flutter' added to the custom LogLevel tag

--- lib/app/utils/loggers/ouisync_app_logger.dart
+++ lib/app/utils/loggers/ouisync_app_logger.dart
@@ -5,7 +5,7 @@ class OuiSyncAppLogger implements LoggyType {
   Loggy<LoggyType> get loggy => Loggy<OuiSyncAppLogger>('[$runtimeType]');
 }
 
-const LogLevel appLevel = LogLevel('ouisync-app', 1);
+const LogLevel appLevel = LogLevel('flutter-ouisync-app', 1);
 
 extension OuiSyncAppLoggy on Loggy {
   void app(dynamic message, [Object? error, StackTrace? stackTrace]) => log(appLevel, message, error, stackTrace);
