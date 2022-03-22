ffac6f3763d92b1cac628e89e4ca891e6bfaf641
Jorge Pabon <jmpabon@gmail.com>
2022-03-22 14:14:54 +0800

    Added: modal_rename_dialog.dart - dialog for renaming entries

---
+++ lib/app/widgets/dialogs/modal_rename_dialog.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import '../../../generated/l10n.dart';
+import '../../utils/utils.dart';
+
+class Rename extends StatelessWidget {
+  Rename({
+    Key? key,
+    required this.context,
+    required this.entryName,
+    required this.hint,
+    required this.formKey,
+  }) : super(key: key);
+
+  final BuildContext context;
+  final String entryName;
+  final String hint;
+  final GlobalKey<FormState> formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: this.formKey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: _buildRenameEntryWidget(this.context),
+    );
+  }
+
+  Widget _buildRenameEntryWidget(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Fields.constrainedText(
+          '\"${this.entryName}\"',
+          flex: 0,
+          fontWeight: FontWeight.w400
+        ),
+        Fields.formTextField(
+          context: context,
+          label: S.current.labelName,
+          hint: this.hint,
+          onSaved: _returnNewName,
+          validator: formNameValidator,
+          autofocus: true
+        ),
+        Fields.actionsSection(
+          context,
+          buttons: _actions(context)),
+      ]
+    );
+  }
+
+  void _returnNewName(String? newName) {
+    final fileExtension = extractFileTypeFromName(this.entryName);
+    if (fileExtension.isNotEmpty) {
+      newName = '$newName.$fileExtension';
+    }
+
+    Navigator.of(this.context).pop(newName);
+  }
+
+  List<Widget> _actions(context) => [
+    ElevatedButton(
+      onPressed: () {
+        if (this.formKey.currentState!.validate()) {
+            this.formKey.currentState!.save();
+          }
+      },
+      child: Text(S.current.actionRename)
+    ),
+    Dimensions.spacingActionsHorizontal,
+    OutlinedButton(
+      onPressed: () => Navigator.of(context).pop(''),
+      child: Text(S.current.actionCancel)
+    ),
+  ];
+}
