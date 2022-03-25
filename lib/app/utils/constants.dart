d459ee987e42bc5e3da3f93f172cb123747a0d9c
Jorge Pabon <jmpabon@gmail.com>
2022-03-25 18:03:17 +0800

    Added: Bittorrent DHT key for persisting each repo status

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -8,6 +8,7 @@ class Constants{
   static const String appDirKey = 'APP_DIR';
   static const String repositoriesDirKey = 'REPOS_DIR';
   static const String currentRepositoryKey = 'CURRENT_REPO';
+  static const String bitTorrentDHTStatusKey = 'BT_DHT';
 
   static const String folderRepositoriesName = 'repositories';
   static const String configuratiosDirName = 'configs';
