d42095c8dd343fd26d91fa2a15cc78420ef2dd28
Peter Jankuliak <p.jankuliak@gmail.com>
2022-04-06 12:03:38 +0200

    Use DataTable for showing connected peers

--- lib/app/pages/peer_list.dart
+++ lib/app/pages/peer_list.dart
@@ -9,25 +9,36 @@ class PeerList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Connected Peers")),
-      body: Container(
-        child: buildList(),
-      )
+      body: Container(child: _buildTable())
     );
   }
 
-  Widget buildList() =>
+  Widget _buildTable() =>
     BlocConsumer<PeerSetCubit, PeerSetChanged>(
-      builder: (context, state) {
-        return ListView.builder(
-          itemCount: state.peers.length,
-          itemBuilder: (context, index) => buildRow(state.peers[index])
-        );},
+      builder: (context, state) =>
+        SingleChildScrollView(
+          // FittexBox to prevent table overflowing the screen.
+          child: FittedBox(
+            fit: BoxFit.fitWidth,
+            child: DataTable(
+              columns: [
+                DataColumn(label: Text("IP")),
+                DataColumn(label: Text("Port")),
+                DataColumn(label: Text("Direction")),
+                DataColumn(label: Expanded(child: Text("State", textAlign: TextAlign.right))),
+              ],
+              rows: state.peers.map((peer) => _buildRow(peer)).toList()))),
       listener: (context, state) {
       });
 
-  Widget buildRow(oui.ConnectedPeer peer) {
-    return ListTile(
-      title: Text("${peer.ip} : ${peer.port}  ${peer.direction}"),
-      trailing: Text("${peer.state}"));
+  DataRow _buildRow(oui.ConnectedPeer peer) {
+    return DataRow(
+      cells: <DataCell>[
+        DataCell(Text(peer.ip)),
+        DataCell(Text(peer.port)),
+        DataCell(Text(peer.direction)),
+        DataCell(Align(alignment: Alignment.centerRight, child: Text(peer.state))),
+      ],
+    );
   }
 }
