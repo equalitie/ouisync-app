8a7b4ba0eb6edb1171f3ec7e8224f80c333d2aac
Jorge Pabon <jmpabon@gmail.com>
2022-06-01 08:55:06 -0500

    Fixed: Cancel validation for saving a file
    The validatiion for canceling saving a file was using only the
    file identifier, which caused that if a file with
    the same name was being saved at the same time in two or more
    repositories, then all of them would be canceled.
    
    Now both the repository and the file identifier are used for the validation.

--- lib/app/bloc/directory/directory_bloc.dart
+++ lib/app/bloc/directory/directory_bloc.dart
@@ -79,9 +79,12 @@ class DirectoryBloc extends Bloc<DirectoryEvent, DirectoryState> with OuiSyncApp
       int offset = 0;
       try {
         final stream = event.fileByteStream
-        .takeWhile((element) { 
-          if (_cancelFileWriting.isEmpty) { return true; }
-          return _cancelFileWriting != event.newFilePath;
+        .takeWhile((element) {
+          return _takeWhile(
+            event.repository.handle,
+            event.newFilePath,
+            _repositorySave,
+            _cancelFileWriting);
         });
         
         await for (final buffer in stream) {
@@ -89,16 +92,19 @@ class DirectoryBloc extends Bloc<DirectoryEvent, DirectoryState> with OuiSyncApp
           offset += buffer.length;
 
           emit(WriteToFileInProgress(
+            repository: event.repository,
             path: event.newFilePath,
             fileName: event.fileName,
             length: event.length,
             progress: offset
           ));
+
+          await Future.delayed(const Duration(seconds: 2));
         }
       } catch (e, st) {
         loggy.app('Writing to file ${event.newFilePath} exception', e, st);
         emit(ShowMessage(S.current.messageWritingFileError(event.newFilePath)));
-        emit(WriteToFileDone(path: event.newFilePath));
+        emit(WriteToFileDone(repository: event.repository, path: event.newFilePath));
         return;
       } finally {
         loggy.app('Writing to file ${event.newFilePath} done - closing');
@@ -107,7 +113,7 @@ class DirectoryBloc extends Bloc<DirectoryEvent, DirectoryState> with OuiSyncApp
 
       if (_cancelFileWriting.isEmpty) {
         emit(ShowMessage(S.current.messageWritingFileDone(event.newFilePath)));
-        emit(WriteToFileDone(path: event.newFilePath));
+        emit(WriteToFileDone(repository: event.repository, path: event.newFilePath));
         return;
       }
 
@@ -116,15 +122,20 @@ class DirectoryBloc extends Bloc<DirectoryEvent, DirectoryState> with OuiSyncApp
         _cancelFileWriting = '';
 
         emit(ShowMessage(S.current.messageWritingFileCanceled(event.newFilePath)));
-        emit(WriteToFileDone(path: event.newFilePath));
+        emit(WriteToFileDone(repository: event.repository, path: event.newFilePath));
       }
     }
   }
 
   String _cancelFileWriting = '';
+  Repository? _repositorySave;
   void _onCancelSaveFile(CancelSaveFile event, Emitter<DirectoryState> emit) {
-    loggy.app('Canceling ${event.filePath}');
+    loggy.app('Canceling ${event.filePath} creation');
+
+    _repositorySave = event.repository.handle;
     _cancelFileWriting = event.filePath;
+
+    loggy.app('Cancel creation: repository=${event.repository.name} handle=${event.repository.handle.handle} file=${event.filePath}');
   }
 
   Future<void> _onDownloadFile(DownloadFile event, Emitter<DirectoryState> emit) async {
@@ -201,18 +212,6 @@ class DirectoryBloc extends Bloc<DirectoryEvent, DirectoryState> with OuiSyncApp
     }
   }
 
-  bool _takeWhile(
-    Repository repository, String filePath,
-    Repository? cancelRepository, String cancelFilePath) {
-      loggy.app('Take while: handle=${repository.handle} file=$filePath cancel-repo=${cancelRepository?.handle} cancel-file=$cancelFilePath');
-
-      if (repository != cancelRepository) {
-        return true;
-      }
-
-      return filePath != cancelFilePath;
-  }  
-
   String _cancelFileDownload = '';
   Repository? _repositoryDownload;
   void _onCancelDownloadFile(CancelDownloadFile event, Emitter<DirectoryState> emit) {
@@ -222,6 +221,20 @@ class DirectoryBloc extends Bloc<DirectoryEvent, DirectoryState> with OuiSyncApp
     _cancelFileDownload = event.filePath;
 
     loggy.app('Cancel downloading: repository=${event.repository.name} handle=${event.repository.handle.handle} file=${event.filePath}');
+  }
+
+  bool _takeWhile(
+    Repository repository,
+    String filePath,
+    Repository? cancelRepository,
+    String cancelFilePath) {
+      loggy.app('Take while: handle=${repository.handle} file=$filePath cancel-repo=${cancelRepository?.handle} cancel-file=$cancelFilePath');
+
+      if (repository != cancelRepository) {
+        return true;
+      }
+
+      return filePath != cancelFilePath;
   }
 
   Future<DirectoryState> _createFile(
