acfa16e66d71cd38e312a42c312c9423f7671cd6
Jorge Pabon <jmpabon@gmail.com>
2022-09-15 15:40:00 +0800

    Added: Uri extension for validating share  links

--- lib/app/utils/extensions.dart
+++ lib/app/utils/extensions.dart
@@ -3,3 +3,17 @@ extension StringExtension on String {
     return '${this[0].toUpperCase()}${substring(1)}';
   }
 }
+
+extension UriExtension on Uri {
+  bool isValidOuiSyncUri() {
+    final isHttps = isScheme('HTTPS');
+    final isOuiSync = host == 'ouisync.net';
+    final hasCorrectPath = path == '/r';
+    final hasToken = hasFragment;
+
+    return isHttps &&
+    hasCorrectPath &&
+    isOuiSync &&
+    hasToken;
+  }
+}
