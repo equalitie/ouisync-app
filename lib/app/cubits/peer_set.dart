d253ae4e80572de457510019958b55799c8ec689
Adam Cigánek <adam.ciganek@proton.me>
2022-10-11 12:43:35 +0200

    Remove redundant bloc dependency - use flutter_bloc instead

--- lib/app/cubits/peer_set.dart
+++ lib/app/cubits/peer_set.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:ouisync_plugin/ouisync_plugin.dart' as oui;
 
