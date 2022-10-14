f0ab5d52f2a322c5c811e899ceeca5403edb5f7e
Jorge Pabon <jmpabon@gmail.com>
2022-10-14 11:27:55 +0800

    Fixed: file extension duplicated when provided by the user
    If the user included the extension when renaming the file, the original
    extension was appened at the end, duplicating it.
    
    Aditionaly, validations to alert the user when an extension is missing
    or changed were added.

--- lib/app/widgets/dialogs/modal_rename_dialog.dart
+++ lib/app/widgets/dialogs/modal_rename_dialog.dart
@@ -5,7 +5,7 @@ import '../../models/item.dart';
 import '../../utils/utils.dart';
 import '../widgets.dart';
 
-class Rename extends StatelessWidget {
+class Rename extends StatefulWidget {
   const Rename({
     Key? key,
     required this.context,
@@ -20,12 +20,44 @@ class Rename extends StatelessWidget {
   final GlobalKey<FormState> formKey;
 
   @override
+  State<Rename> createState() => _RenameState();
+}
+
+class _RenameState extends State<Rename> {
+  late String _oldName;
+
+  final _newNameController = TextEditingController();
+
+  late String _originalExtension;
+
+  late bool _isFile;
+
+  @override
+  void initState() {
+    selectEntryName();
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Form(
-      key: formKey,
+      key: widget.formKey,
       autovalidateMode: AutovalidateMode.onUserInteraction,
-      child: _buildRenameEntryWidget(this.context),
+      child: _buildRenameEntryWidget(widget.context),
     );
+  }
+
+  void selectEntryName() {
+    _oldName = getBasename(widget.entryData.path);
+    _originalExtension = getFileExtension(widget.entryData.path);
+
+    _isFile = widget.entryData is FileItem;
+    final fileExtensionOffset = _isFile ? _originalExtension.length : 0;
+
+    _newNameController.text = _oldName;
+    _newNameController.selection = TextSelection(
+        baseOffset: 0, extentOffset: _oldName.length - fileExtensionOffset);
   }
 
   Widget _buildRenameEntryWidget(BuildContext context) {
@@ -34,40 +66,91 @@ class Rename extends StatelessWidget {
         mainAxisSize: MainAxisSize.max,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Fields.constrainedText('"$entryName"',
+          Fields.constrainedText('"$_oldName"',
               flex: 0, fontWeight: FontWeight.w400),
           Fields.formTextField(
               context: context,
+              textEditingController: _newNameController,
               label: S.current.labelName,
-              hint: hint,
-              onSaved: _returnNewName,
-              validator: validateNoEmpty(
-                  S.current.messageErrorFormValidatorNameDefault),
+              hint: widget.hint,
+              onSaved: (newName) => Navigator.of(context).pop(newName),
+              validator: _validateEntryName(
+                  emptyError: S.current.messageErrorFormValidatorNameDefault,
+                  regExp: '.*[/\\\\].*',
+
+                  /// No / nor \ allowed
+                  regExpError: 'Using \\ or / is not allowed'),
               autofocus: true),
           Fields.dialogActions(context, buttons: _actions(context)),
         ]);
   }
 
-  void _returnNewName(String? newName) {
-    final fileExtension = getFileExtension(entryName);
-    if (fileExtension.isNotEmpty) {
-      newName = '$newName$fileExtension';
-    }
+  String? Function(String?) _validateEntryName(
+          {String? emptyError, String? regExp, String? regExpError}) =>
+      (String? newName) {
+        if (newName?.isEmpty ?? true) return emptyError;
+        if (newName!.contains(RegExp(regExp!))) return regExpError;
 
-    Navigator.of(context).pop(newName);
-  }
+        return null;
+      };
 
   List<Widget> _actions(context) => [
         NegativeButton(
             text: S.current.actionCancel,
             onPressed: () => Navigator.of(context).pop('')),
         PositiveButton(
-            text: S.current.actionRename, onPressed: _validateNewName)
+            text: S.current.actionRename,
+            onPressed: () async =>
+                await _validateNewName(_newNameController.text))
       ];
 
-  void _validateNewName() {
-    if (formKey.currentState!.validate()) {
-      formKey.currentState!.save();
+  Future<void> _validateNewName(String newName) async {
+    if (!(widget.formKey.currentState?.validate() ?? false)) return;
+
+    if (_isFile) {
+      final extensionValidationOK = await _validateExtension(newName);
+      if (!extensionValidationOK) return;
     }
+
+    widget.formKey.currentState!.save();
+  }
+
+  Future<bool> _validateExtension(String name) async {
+    final fileExtension = getFileExtension(name);
+
+    /// If there was not extension originally, then no need to have or validate a new one
+    if (_originalExtension.isEmpty) return true;
+
+    String title = '';
+    String message = 'Changing the extension of a file can make it unnusable';
+
+    if (fileExtension != _originalExtension) {
+      title = 'File extension changed';
+    }
+
+    if (fileExtension.isEmpty) {
+      title = 'File extension missing';
+    }
+
+    if (title.isEmpty) return true;
+
+    final continueAnyway = await Dialogs.alertDialogWithActions(
+        context: widget.context,
+        title: title,
+        body: [
+          Text(message)
+        ],
+        actions: [
+          TextButton(
+            child: Text(S.current.actionRename.toUpperCase()),
+            onPressed: () => Navigator.of(widget.context).pop(true),
+          ),
+          TextButton(
+            child: Text(S.current.actionCancelCapital),
+            onPressed: () => Navigator.of(widget.context).pop(false),
+          )
+        ]);
+
+    return continueAnyway ?? false;
   }
 }
