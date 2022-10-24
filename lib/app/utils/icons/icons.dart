029c3d556a7e84a88d91477e07a3d5dcc8afc765
Jorge Pabon <jmpabon@gmail.com>
2022-10-24 16:17:22 +0800

    Added: icons.dart

---
+++ lib/app/utils/icons/icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class FileIcons {
+  static final Map _icons = <String, IconData>{
+    ".apk": FontAwesomeIcons.android
+  };
+}
