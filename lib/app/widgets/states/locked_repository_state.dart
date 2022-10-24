6cb64df9fc480712d87cd73162feb6a18f23b621
Jorge Pabon <jmpabon@gmail.com>
2022-10-24 11:53:18 +0800

    Fixed: A RenderFlex overflowed by 27 pixels on the bottom
    When a modal dialog was pushed on top of the locked repo state, the
    keyboard caused the UI exception

--- lib/app/widgets/states/locked_repository_state.dart
+++ lib/app/widgets/states/locked_repository_state.dart
@@ -15,46 +15,45 @@ class LockedRepositoryState extends StatelessWidget {
   Widget build(BuildContext context) {
     final lockedRepoImageHeight = MediaQuery.of(context).size.height * 0.2;
 
-    return Container(
-      padding: Dimensions.paddingContents,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Align(
-              alignment: Alignment.center,
-              child: Fields.placeholderWidget(
-                  assetName: Constants.assetLockedRepository,
-                  assetHeight: lockedRepoImageHeight)),
-          Dimensions.spacingVerticalDouble,
-          Align(
-              alignment: Alignment.center,
-              child: Fields.inPageMainMessage(S.current.messageLockedRepository,
-                  tags: {
-                    Constants.inlineTextColor:
-                        InlineTextStyles.color(Colors.black),
-                    Constants.inlineTextSize: InlineTextStyles.size(),
-                    Constants.inlineTextBold: InlineTextStyles.bold
-                  })),
-          Dimensions.spacingVertical,
-          Align(
-              alignment: Alignment.center,
-              child: Fields.inPageSecondaryMessage(
-                  S.current.messageInputPasswordToUnlock,
-                  tags: {
-                    Constants.inlineTextSize: InlineTextStyles.size(),
-                    Constants.inlineTextBold: InlineTextStyles.bold,
-                  })),
-          Dimensions.spacingVerticalDouble,
-          Fields.inPageButton(
-              onPressed: () {
-                onUnlockPressed!.call(repositoryName);
-              },
-              leadingIcon: const Icon(Icons.lock_open_rounded),
-              text: S.current.actionUnlock,
-              autofocus: true)
-        ],
-      ),
-    );
+    return Center(
+        child: SingleChildScrollView(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Align(
+            alignment: Alignment.center,
+            child: Fields.placeholderWidget(
+                assetName: Constants.assetLockedRepository,
+                assetHeight: lockedRepoImageHeight)),
+        Dimensions.spacingVerticalDouble,
+        Align(
+            alignment: Alignment.center,
+            child: Fields.inPageMainMessage(S.current.messageLockedRepository,
+                tags: {
+                  Constants.inlineTextColor:
+                      InlineTextStyles.color(Colors.black),
+                  Constants.inlineTextSize: InlineTextStyles.size(),
+                  Constants.inlineTextBold: InlineTextStyles.bold
+                })),
+        Dimensions.spacingVertical,
+        Align(
+            alignment: Alignment.center,
+            child: Fields.inPageSecondaryMessage(
+                S.current.messageInputPasswordToUnlock,
+                tags: {
+                  Constants.inlineTextSize: InlineTextStyles.size(),
+                  Constants.inlineTextBold: InlineTextStyles.bold,
+                })),
+        Dimensions.spacingVerticalDouble,
+        Fields.inPageButton(
+            onPressed: () {
+              onUnlockPressed!.call(repositoryName);
+            },
+            leadingIcon: const Icon(Icons.lock_open_rounded),
+            text: S.current.actionUnlock,
+            autofocus: true)
+      ],
+    )));
   }
 }
