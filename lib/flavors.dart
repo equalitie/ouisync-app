09ac9d93e975f54113fc154920a90a97be71a23a
Jorge Pabon <jmpabon@gmail.com>
2022-10-28 18:55:42 +0800

    Added: development flavor
    Also:
    
    - Development flavor adds an extra section to the applicationId: .dev
    - Development flavor suffix: -dev
    - Analytics flavor uses default applicationId

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
-enum Flavor {
-  vanilla,
-  analytics,
-}
+enum Flavor { vanilla, analytics, development }
 
 class F {
   static Flavor? appFlavor;
@@ -17,8 +14,10 @@ class F {
         return 'OuiSync';
       case Flavor.analytics:
         return 'OuiSync';
+      case Flavor.development:
+        return 'OuiSync-Dev';
       default:
-        return 'title';
+        return 'OuiSync';
     }
   }
 
@@ -27,6 +26,8 @@ class F {
       case Flavor.vanilla:
         return null;
       case Flavor.analytics:
+        return Colors.orange.shade800;
+      case Flavor.development:
         return Colors.grey.shade800;
       default:
         return null;
