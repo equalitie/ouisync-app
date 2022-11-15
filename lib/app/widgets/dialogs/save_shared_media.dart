2804694b13850a83f9cfddd3e89f830aa89b018c
Jorge Pabon <jmpabon@gmail.com>
2022-11-15 13:16:09 +0800

    UI for adding files to OuiSync updated
    - Title centered
    - List view with scroll for multiple files
    - Vertical size constrained
    - Improved alerts for no repo or locked repo

--- lib/app/widgets/dialogs/save_shared_media.dart
+++ lib/app/widgets/dialogs/save_shared_media.dart
@@ -13,56 +13,94 @@ class SaveSharedMedia extends StatelessWidget {
     required this.sharedMedia,
     required this.onBottomSheetOpen,
     required this.onSaveFile,
+    required this.validationFunction,
   });
 
   final List<SharedMediaFile> sharedMedia;
   final BottomSheetControllerCallback onBottomSheetOpen;
   final SaveFileCallback onSaveFile;
+  final Future<bool> Function() validationFunction;
 
   @override
   Widget build(BuildContext context) {
+    final nediaListMaxHeight = MediaQuery.of(context).size.height * 0.2;
+
     return Container(
       padding: Dimensions.paddingBottomSheet,
-      height: 180.0,
       decoration: Dimensions.decorationBottomSheetAlternative,
       child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-                Fields.iconLabel(
-                  icon: Icons.save_outlined,
-                  text: S.current.titleAddFile,
-                )
-              ] +
-              sharedMedia.map((media) {
-                return Fields.constrainedText(getBasename(media.path),
-                    softWrap: true,
-                    textOverflow: TextOverflow.ellipsis,
-                    fontWeight: FontWeight.w800);
-              }).toList() +
-              [
-                Fields.dialogActions(context,
-                    buttons: _actions(context),
-                    padding: const EdgeInsets.only(top: 0.0),
-                    mainAxisAlignment: MainAxisAlignment.end)
-              ]),
+            Fields.bottomSheetTitle(S.current.titleAddFile,
+                textAlign: TextAlign.start),
+            ConstrainedBox(
+                constraints:
+                    BoxConstraints.loose(Size.fromHeight(nediaListMaxHeight)),
+                child: _buildMediaList(context, sharedMedia)),
+            Fields.dialogActions(context,
+                buttons: _actions(context),
+                padding: const EdgeInsets.only(top: 20.0),
+                mainAxisAlignment: MainAxisAlignment.end)
+          ]),
     );
   }
+
+  Widget _buildMediaList(BuildContext context, List<SharedMediaFile> media) =>
+      ListView.separated(
+          shrinkWrap: true,
+          separatorBuilder: (context, index) =>
+              const Divider(height: 1, color: Colors.black12),
+          itemCount: media.length,
+          itemBuilder: (context, index) {
+            final mediaItem = media[index];
+            final name = getBasename(mediaItem.path);
+
+            return Container(
+                padding: Dimensions.paddingListItem,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                            flex: 1,
+                            child: const Icon(Icons.insert_drive_file_outlined,
+                                size: Dimensions.sizeIconAverage)),
+                        Expanded(
+                            flex: 9,
+                            child: Fields.autosizeText(name,
+                                fontWeight: FontWeight.w800))
+                      ],
+                    ),
+                    Fields.autosizeText(mediaItem.path)
+                  ],
+                ));
+          });
 
   List<Widget> _actions(BuildContext context) => [
         NegativeButton(
             text: S.current.actionCancel,
-            onPressed: () => _cancelSaveFile(context)),
+            onPressed: () => _cancelSaveFile(context),
+            buttonsAspectRatio: Dimensions.aspectRatioBottomDialogButton),
         PositiveButton(
             text: S.current.actionSave,
             onPressed: () async {
+              final canSaveMedia = await validationFunction();
+              if (!canSaveMedia) {
+                return;
+              }
+
               for (final media in sharedMedia) {
-                onSaveFile(media.path);
+                await onSaveFile(media.path);
               }
 
               Navigator.of(context).pop();
-            })
+            },
+            buttonsAspectRatio: Dimensions.aspectRatioBottomDialogButton)
       ];
 
   void _cancelSaveFile(context) {
