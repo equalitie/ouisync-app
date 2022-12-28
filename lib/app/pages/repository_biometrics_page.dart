b894a43ff79e5568cba45d8bef45f24bb9ea850f
Jorge Pabon <jmpabon@gmail.com>
2022-12-28 18:02:29 +0800

    Added: RepositoryBiometrics page
    - Remove the repository password from biometrics.
    
    - Manage password: see/copy password in biometric storage.
    
    - Add repository password to biometric storage (pending).

---
+++ lib/app/pages/repository_biometrics_page.dart
@@ -0,0 +1,256 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:ouisync_plugin/ouisync_plugin.dart';
+
+import '../../generated/l10n.dart';
+import '../cubits/repos.dart';
+import '../utils/loggers/ouisync_app_logger.dart';
+import '../utils/utils.dart';
+import '../widgets/widgets.dart';
+
+class RepositoryBiometrics extends StatefulWidget {
+  const RepositoryBiometrics(
+      {required this.repositoryName,
+      required this.repositories,
+      required this.biometrics,
+      super.key});
+
+  final String repositoryName;
+  final ReposCubit repositories;
+  final bool biometrics;
+
+  @override
+  State<RepositoryBiometrics> createState() => _RepositoryBiometricsState();
+}
+
+class _RepositoryBiometricsState extends State<RepositoryBiometrics>
+    with OuiSyncAppLogger {
+  String? _password;
+
+  bool _usesBiometrics = false;
+  bool _managePassword = false;
+  bool _previewPassword = false;
+
+  bool _removeBiometrics = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _usesBiometrics = widget.biometrics;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: Text('Biometrics'),
+        elevation: 0.0,
+      ),
+      body: _biometricsState());
+  // body: _password?.isNotEmpty ?? false
+  //     ? _biometricsState()
+  //     : _noPasswordState());
+
+  Widget _noPasswordState() {
+    return Container(
+        child: Column(
+      children: [],
+    ));
+  }
+
+  Widget _biometricsState() {
+    return SingleChildScrollView(
+        child: Container(
+            child: Column(
+      children: [
+        SwitchListTile.adaptive(
+            value: _usesBiometrics,
+            title: Text('Unlock using biometrics'),
+            onChanged: (useBiometrics) async =>
+                await _unlockUsingBiometrics(useBiometrics)),
+        Divider(),
+        ..._usesBiometrics ? _manageBiometrics() : _addBiometrics(),
+        Divider(),
+        ListTile(
+          title: Text('Repository name'),
+          subtitle: Text(widget.repositoryName),
+        ),
+        Divider()
+      ],
+    )));
+  }
+
+  List<Widget> _manageBiometrics() {
+    return [
+      Visibility(
+          visible: _usesBiometrics,
+          child: SwitchListTile.adaptive(
+              value: _managePassword,
+              title: Text('Manage password'),
+              onChanged: (enableManagement) async {
+                if (!enableManagement) {
+                  setState(() {
+                    _managePassword = enableManagement;
+                    _password = '';
+                  });
+                  return;
+                }
+
+                String? biometricPassword;
+                try {
+                  biometricPassword = await Biometrics.getRepositoryPassword(
+                      repositoryName: widget.repositoryName);
+                } catch (e) {
+                  loggy.app(e);
+                  return;
+                }
+
+                if (biometricPassword?.isEmpty ?? true) return;
+
+                setState(() {
+                  _password = biometricPassword;
+                  _managePassword = enableManagement;
+                });
+              })),
+      ListTile(
+        title: Text(
+          'Password',
+        ),
+        subtitle: Text(_formattPassword(_password, mask: !_previewPassword)),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+                flex: 0,
+                child: IconButton(
+                    onPressed: _managePassword
+                        ? () =>
+                            setState(() => _previewPassword = !_previewPassword)
+                        : null,
+                    icon: _previewPassword
+                        ? const Icon(Constants.iconVisibilityOff)
+                        : const Icon(Constants.iconVisibilityOn),
+                    padding: EdgeInsets.zero,
+                    color: Theme.of(context).primaryColor)),
+            Expanded(
+                flex: 0,
+                child: IconButton(
+                    onPressed: _managePassword
+                        ? () async {
+                            if (_password == null) return;
+
+                            await copyStringToClipboard(_password!);
+                            showSnackBar(context,
+                                content: Text(
+                                    '"${widget.repositoryName}" password copied to the clipboard'));
+                          }
+                        : null,
+                    icon: const Icon(Icons.copy_rounded),
+                    padding: EdgeInsets.zero,
+                    color: Theme.of(context).primaryColor))
+          ],
+        ),
+      ),
+      Visibility(
+          visible: _removeBiometrics,
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+              child: Text(
+                  'Once you navigate out of this screen you wont be able to see or copy '
+                  'the password anymore; please save it in a secure place.',
+                  style: TextStyle(color: Colors.red)))),
+    ];
+  }
+
+  String _formattPassword(String? password, {bool mask = true}) =>
+      (mask ? "*" * (password ?? '').length : password) ?? '';
+
+  List<Widget> _addBiometrics() {
+    return [];
+  }
+
+  // Removing a repository from the biometrics storage:
+  // After authenticating using biometrics, we remove the password from the
+  // biometric storage and enable the "Manage password" section so the user
+  // can see / copy the password and saved it on its own.
+  // We also display a meesage telling the user to do this.
+  Future<void> _unlockUsingBiometrics(bool useBiometrics) async => useBiometrics
+      ? await _addRepoBiometrics()
+      : await _removeRepoBiometrics();
+
+  Future<void> _addRepoBiometrics() async {
+    final initialAccessMode =
+        widget.repositories.currentRepo?.maybeCubit?.accessMode ??
+            AccessMode.blind;
+
+    final passwordValidation = await showDialog<bool?>(
+        context: context,
+        builder: (BuildContext context) => ActionsDialog(
+              title: S.current.messageUnlockRepository,
+              body: UnlockRepository(
+                  context: context,
+                  repositoryName: widget.repositoryName,
+                  useBiometrics: true,
+                  unlockRepositoryCallback: _unlockRepository),
+            ));
+
+    if (!(passwordValidation ?? false)) return;
+
+    if (initialAccessMode == AccessMode.blind) {
+      await _unlockRepository(
+          repositoryName: widget.repositoryName, password: '');
+    }
+
+    setState(() {
+      _usesBiometrics = passwordValidation!;
+      _managePassword = !passwordValidation;
+    });
+  }
+
+  Future<void> _removeRepoBiometrics() async {
+    final removeBiometrics = await _removeBiometricsDialog();
+    if (!(removeBiometrics ?? false)) return;
+
+    final authenticatedOk = await LocalAuthentication()
+        .authenticate(localizedReason: 'Authenticate first...');
+
+    if (authenticatedOk) {
+      await Biometrics.deleteRepositoryPassword(
+          repositoryName: widget.repositoryName);
+
+      setState(() {
+        _removeBiometrics = true;
+
+        _usesBiometrics = false;
+        _managePassword = true;
+      });
+    }
+  }
+
+  Future<AccessMode?> _unlockRepository(
+          {required String repositoryName, required String password}) async =>
+      await widget.repositories
+          .unlockRepository(repositoryName, password: password);
+
+  Future<bool?> _removeBiometricsDialog() async =>
+      await Dialogs.alertDialogWithActions(
+          context: context,
+          title: 'Remove biometrics',
+          body: [
+            Text('Are you sure you want to remove this repository biometrics?')
+          ],
+          actions: [
+            TextButton(
+              child: Text(S.current.actionAccept),
+              onPressed: () => Navigator.of(context).pop(true),
+            ),
+            TextButton(
+              child: Text(S.current.actionCancel),
+              onPressed: () => Navigator.of(context).pop(false),
+            )
+          ]);
+}
