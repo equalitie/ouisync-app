673f59ff3be3f8d4568af27165efb1704d3b8399
Jorge Pabon <jmpabon@gmail.com>
2023-02-15 10:38:10 +0800

    Added: Platform clasess
    - PlatformWidget: builds a widget for mobile or desktop.
    - PlatformValues: Utility class for getting platform, screen size and
    form factor.
    - FormFactor: Utility class with dimensions and calculations for each
    form factor.

--- lib/app/utils/platform/platform.dart
+++ lib/app/utils/platform/platform.dart
@@ -7,6 +7,8 @@ export 'platform_background_manager.dart';
 export 'platform_background_manager_mobile.dart';
 export 'platform_background_manager_desktop.dart';
 
+export 'platform_widget.dart';
+
 export 'platform_window_manager.dart';
 export 'platform_window_manager_mobile.dart';
 export 'platform_window_manager_desktop.dart';
