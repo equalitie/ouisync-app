8681aaaef9076c731a19c1ac11af2713ac9b4558
Jorge Pabon <jmpabon@gmail.com>
2023-02-16 17:54:48 +0800

    settings_desktop_item.dart added to the barrel file + name update for the container

--- lib/app/widgets/widgets.dart
+++ lib/app/widgets/widgets.dart
@@ -40,12 +40,13 @@ export 'states/missing_repo_state.dart';
 export 'states/no_contents_state.dart';
 export 'states/no_repositories_state.dart';
 
-export 'settings/desktop/settings_desktop_container.dart';
+export 'settings/desktop/setting_desktop_item.dart';
 export 'settings/desktop/settings_desktop_detail.dart';
-export 'settings/desktop/settings_desktop_main.dart';
+export 'settings/desktop/settings_desktop_list.dart';
 export 'settings/about_section_mobile.dart';
 export 'settings/logs_section_mobile.dart';
 export 'settings/network_section_mobile.dart';
 export 'settings/repository_section_mobile.dart';
+export 'settings/settings_container.dart';
 
 export 'log_view.dart';
