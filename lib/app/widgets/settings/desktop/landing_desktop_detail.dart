9980ae6eb011f4415cb387667f499556228460af
Jorge Pabon <jmpabon@gmail.com>
2023-02-17 13:40:39 +0800

    Fixed: sizedbox removed from landing widget on desktop settings.

--- lib/app/widgets/settings/desktop/landing_desktop_detail.dart
+++ lib/app/widgets/settings/desktop/landing_desktop_detail.dart
@@ -3,6 +3,6 @@ import 'package:flutter/widgets.dart';
 class LandingDesktopDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(child: SizedBox.expand(child: Text('None selected')));
+    return Container(child: Text('None selected'));
   }
 }
