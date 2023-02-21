cb2ca77820ba71c92f29992732d85eebf2f59e70
Jorge Pabon <jmpabon@gmail.com>
2023-02-21 15:51:20 +0800

    app_version_tile_mobile.dart refactored to be used on mobile and desktop.
    - Renamed: app_version_tile.dart
    - Class: renamed as AppVersionTile

---
+++ lib/app/widgets/settings/app_version_tile.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ouisync_plugin/ouisync_plugin.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:settings_ui/settings_ui.dart';
+
+import '../../../generated/l10n.dart';
+import '../../cubits/cubits.dart';
+import '../../pages/pages.dart';
+import '../../utils/click_counter.dart';
+import '../../utils/platform/platform_values.dart';
+
+class AppVersionTile extends StatefulWidget {
+  final Session session;
+  final Widget title;
+  final Widget? leading;
+
+  AppVersionTile({required this.session, required this.title, this.leading});
+
+  @override
+  State<AppVersionTile> createState() => _AppVersionTileState();
+}
+
+class _AppVersionTileState extends State<AppVersionTile> {
+  late Future<String> _version;
+  final _clickCounter = ClickCounter(timeoutMs: 3000);
+
+  @override
+  void initState() {
+    super.initState();
+    _version = PackageInfo.fromPlatform().then((info) => info.version);
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      PlatformValues.isMobileDevice ? _buildMobileTile() : _buildDesktopTile();
+
+  SettingsTile _buildMobileTile() => SettingsTile(
+      leading: widget.leading,
+      title: widget.title,
+      value: _getAppVersion(),
+      onPressed: _onPressed);
+
+  Widget _buildDesktopTile() => ListTile(
+      leading: widget.leading,
+      title: widget.title,
+      subtitle: _getAppVersion(),
+      onTap: () => _onPressed);
+
+  FutureBuilder<String> _getAppVersion() => FutureBuilder<String>(
+        future: _version,
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          late Widget version;
+
+          if (snapshot.hasData) {
+            version = Text(snapshot.data!);
+          } else if (snapshot.hasError) {
+            version = Text("???");
+          } else {
+            version = Text("...");
+          }
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              version,
+              BlocBuilder<UpgradeExistsCubit, bool>(
+                builder: (context, state) {
+                  if (state) {
+                    return Text(
+                      S.current.messageNewVersionIsAvailable,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    );
+                  } else {
+                    return SizedBox.shrink();
+                  }
+                },
+              ),
+            ],
+          );
+        },
+      );
+
+  void _onPressed(BuildContext context) {
+    if (_clickCounter.registerClick() >= 3) {
+      _clickCounter.reset();
+
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => StateMonitorPage(widget.session)));
+    }
+  }
+}
