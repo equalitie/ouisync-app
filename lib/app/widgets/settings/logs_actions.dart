7813b1edf5a9224ec23b12b20f0a1c251630e4c8
Adam Cigánek <adam.ciganek@proton.me>
2023-06-19 11:06:45 +0200

    Save logs to Downloads dir by default

--- lib/app/widgets/settings/logs_actions.dart
+++ lib/app/widgets/settings/logs_actions.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_file_dialog/flutter_file_dialog.dart';
 import 'package:ouisync_plugin/state_monitor.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
-import 'package:file_picker/file_picker.dart';
 
 import '../../utils/dump.dart';
 import '../../pages/log_view_page.dart';
@@ -22,8 +24,10 @@ class LogsActions {
         final params = SaveFileDialogParams(sourceFilePath: tempFile.path);
         await FlutterFileDialog.saveFile(params: params);
       } else if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
-        final outputPath =
-            await FilePicker.platform.saveFile(fileName: tempFile.path);
+        final initialDir = await getDownloadsDirectory();
+        final outputPath = await FilePicker.platform.saveFile(
+            fileName: basename(tempFile.path),
+            initialDirectory: initialDir?.path);
 
         if (outputPath != null) {
           await tempFile.copy(outputPath);
