86b964d59f3eca57329029a8bc8116b136428362
Jorge Pabon <jmpabon@gmail.com>
2023-06-19 16:25:13 -0500

    FAQ page replaced with webview loading url with FAQ in the repo

--- lib/app/widgets/settings/about_section_mobile.dart
+++ lib/app/widgets/settings/about_section_mobile.dart
@@ -6,6 +6,8 @@ import 'package:settings_ui/settings_ui.dart';
 import '../../../generated/l10n.dart';
 import '../../cubits/cubits.dart';
 import '../../pages/pages.dart';
+import '../../utils/platform/platform.dart';
+import '../../utils/utils.dart';
 import 'app_version_tile.dart';
 
 class AboutSectionMobile extends AbstractSettingsSection {
@@ -21,8 +23,18 @@ class AboutSectionMobile extends AbstractSettingsSection {
               title: Text("FAQ"),
               leading: Icon(Icons.question_answer_rounded),
               value: Text('Frequently Asked Questions'),
-              onPressed: (context) => Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => FAQPage()))),
+              onPressed: (context) async {
+                final title = Text('FAQ');
+                final content = await Dialogs.executeFutureWithLoadingDialog(
+                    context,
+                    f: PlatformWebView().loadUrl(context, Constants.faqUrl));
+
+                await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            WebViewPage(title: title, content: content)));
+              }),
           CustomSettingsTile(
             child: AppVersionTile(
               session: repos.session,
