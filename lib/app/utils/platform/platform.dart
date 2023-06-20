dc114200195e07bc86b9fc368293f3e5d21457d1
Jorge Pabon <jmpabon@gmail.com>
2023-06-19 21:11:50 -0500

    Added: platform_webview.dart, platform_webview_desktop.dart, platform_webview_mobile.dart
    (cherry picked from commit 60ab81e70ce7d2c37661cf5079ebccaaf575581e)

--- lib/app/utils/platform/platform.dart
+++ lib/app/utils/platform/platform.dart
@@ -10,6 +10,10 @@ export 'platform_background_manager_desktop.dart';
 export 'platform_values.dart';
 export 'platform_widget.dart';
 
+export 'platform_webview_desktop.dart';
+export 'platform_webview_mobile.dart';
+export 'platform_webview.dart';
+
 export 'platform_window_manager.dart';
 export 'platform_window_manager_mobile.dart';
 export 'platform_window_manager_desktop.dart';
