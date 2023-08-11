6777d48d9f192353c139a9579dd81c31f7500401
Jorge Pabon <jmpabon@gmail.com>
2023-08-11 14:30:41 +0800

    Linter: Unused import

--- lib/app/cubits/background_service_manager.dart
+++ lib/app/cubits/background_service_manager.dart
@@ -7,7 +7,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../generated/l10n.dart';
 import '../utils/utils.dart';
-import '../widgets/widgets.dart';
 
 class BackgroundServiceManagerState {
   bool isServiceRunning;
