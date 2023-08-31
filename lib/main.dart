8b2d1360b5a3b4b01f8dd570bb2c73240cb4c3b0
Jorge Pabon <jmpabon@gmail.com>
2023-08-31 18:19:59 +0800

    Sentry initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'app/app.dart';
 
 Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final app = await initOuiSyncApp(args);
-  runApp(app);
+  final dsn = '';
+  await setupSentry(() async => runApp(await initOuiSyncApp(args)), dsn);
+}
+
+Future<void> setupSentry(AppRunner ouisyncAppRunner, String ouisyncDSN,
+    {bool isIntegrationTest = false,
+    BeforeSendCallback? beforeSendCallback}) async {
+  await SentryFlutter.init((options) {
+    options
+      ..dsn = ouisyncDSN
+      ..tracesSampleRate = 1.0;
+
+    if (isIntegrationTest) {
+      options
+        ..dist = '1'
+        ..environment = 'integration'
+        ..beforeSend = beforeSendCallback;
+    }
+  }, appRunner: ouisyncAppRunner);
 }
