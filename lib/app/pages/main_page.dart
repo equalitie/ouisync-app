2d11f78608dcfdd8d1b5014ef83b9586e53afb34
Jorge Pabon <jmpabon@gmail.com>
2023-09-25 14:37:16 +0800

    [Android] Fix: requesting validation for navigating to the app settings
    This are the general app settings, not the repo-specific settings, so no
     validation is required on mobile

--- lib/app/pages/main_page.dart
+++ lib/app/pages/main_page.dart
@@ -320,8 +320,10 @@ class _MainPageState extends State<MainPage>
   Widget _buildAppSettingsIcon() {
     final button = Fields.actionIcon(const Icon(Icons.settings_outlined),
         onPressed: () async {
-      final authorized = await authorizeNavigationToSettings();
-      if (authorized == null || authorized == false) return;
+      if (PlatformValues.isDesktopDevice) {
+        final authorized = await authorizeNavigationToSettings();
+        if (authorized == null || authorized == false) return;
+      }
 
       await _showAppSettings();
     }, size: Dimensions.sizeIconSmall);
