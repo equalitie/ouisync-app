847a30b2f66a15fdea446d074dec0f6d179d1ab8
Jorge Pabon <jmpabon@gmail.com>
2023-09-26 09:13:47 +0800

    Added: app_icon.png added to assets for Linux system tray icon

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -158,7 +158,7 @@ class Constants {
   static const String onboardingAccessImage =
       '03_onboarding_access_multiple_devices.png';
 
-  static const String appIcon = './assets/app_icon.png';
+  static const String appIcon = 'assets/app_icon.png';
   static const String windowsAppIcon = './assets/app_icon.ico';
 
   static const String eventLeftMouseUp = 'leftMouseUp';
