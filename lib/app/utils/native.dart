464d5fbc3b64d0f2cb22c096687b535e8e03ff68
Jorge Pabon <jmpabon@gmail.com>
2024-01-22 20:30:40 -0500

    Add Native class for handling method channel calls: getDownloadPathForAndroid

---
+++ lib/app/utils/native.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/services.dart';
+
+/// MethodChannel handler for calling functions
+/// implemented natively, and viceversa.
+class Native {
+  static const MethodChannel _channel =
+      MethodChannel('org.equalitie.ouisync_app/native');
+
+  /// Sets the method handler for the calls to and from native implementations.
+  static void init() {
+    _channel.setMethodCallHandler(_methodHandler);
+  }
+
+  /// Handler method in charge of picking the right function based in the
+  /// [call.method].
+  ///
+  /// [call] is the object sent from the native platform with the function name ([call.method])
+  /// and any arguments included ([call.arguments])
+  static Future<dynamic> _methodHandler(MethodCall call) async {}
+
+  /// In Android, it retrieves the legacy path to the Download directory
+  static Future<String> getDownloadPathForAndroid() async {
+    final dynamic result = await _channel.invokeMethod('getDownloadPath');
+    return result;
+  }
+}
