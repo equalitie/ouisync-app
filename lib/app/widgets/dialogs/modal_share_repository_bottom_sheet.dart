3a76089103c51b63850a38c168ed6ae6f410b608
Jorge Pabon <jmpabon@gmail.com>
2024-01-25 17:07:59 -0500

    Code formatting

--- lib/app/widgets/dialogs/modal_share_repository_bottom_sheet.dart
+++ lib/app/widgets/dialogs/modal_share_repository_bottom_sheet.dart
@@ -215,12 +215,15 @@ class _ShareRepositoryState extends State<ShareRepository> with AppLogger {
                 }
 
                 Rect? origin;
+
                 /// We need the sharePositionOrigin parameter for iPads and macOS, or it would throw an exception.
                 if (Platform.isIOS || Platform.isMacOS) {
                   final renderBox = context.findRenderObject() as RenderBox?;
-                  origin = (renderBox?.localToGlobal(Offset.zero) ?? Offset(0.0, 0.0)) & (renderBox?.size ?? Size.zero);  
+                  origin = (renderBox?.localToGlobal(Offset.zero) ??
+                          Offset(0.0, 0.0)) &
+                      (renderBox?.size ?? Size.zero);
                 }
-              
+
                 await Share.share(_shareToken!, sharePositionOrigin: origin);
               }),
               Fields.constrainedText(S.current.labelShareLink,
