730e15ecbee0fd0368d9f61c45b55079a1ba5a26
Jorge Pabón <J-Pabon@users.noreply.github.com>
2024-02-26 18:03:15 +0800

    Update the Dokan installation script to handle multiple driver versions

--- scripts/install_dokan2.ps1
+++ scripts/install_dokan2.ps1
@@ -6,47 +6,60 @@ $full_minimum_ouisync_dokan_version = "2.1.0.1000"
 
 $root_directory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
-[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-
 ######################
 # FUNCTIONS          #
 ######################
 
-function Get-DokanLatestVersionInSystem {
-    $latest_dokan_driver_version = (Get-ChildItem -Path "$env:WINDIR\system32\drivers" -Filter dokan*.sys).VersionInfo.FileVersionRaw | Sort-Object -Descending | Select-Object -First 1
+function Get-DokanVersionsInSystem {
+    $dokan_driver_versions = (Get-ChildItem -Path "$env:WINDIR\system32\drivers" -Filter dokan*.sys).VersionInfo.FileVersionRaw | Sort-Object -Descending | Get-Unique
 
-    if ($null -ne $latest_dokan_driver_version) {
-        Write-Host "Dokan driver version $latest_dokan_driver_version"
-    } else {
-        Write-Host "No Dokan drivers found"
-    }
-
-    return $latest_dokan_driver_version
+    return $dokan_driver_versions
 }
 
-
-function Get-IsCurrentDokanVersionSupported {
+function Get-IsDokanInstallRequired {
     Param(
-        [String] $DokanVersion
+        [String[]] $CurrentDokanVersions
     )
 
-    $version_array = $DokanVersion.replace(".", ":") -split ":"
-    
-    $MAYOR = $version_array[0]
-    $MINOR = $version_array[1]
-    $PATCH = $version_array[2]
-
-    if ("$MAYOR.$MINOR.$PATCH" -eq $minimum_ouisync_dokan_version -or
-        $MAYOR -gt $ouisync_dokan_mayor) {
-        return $true
+    # Select WHERE the version string starts with 2.* to only select version 2 of the driver.
+    $dokan2_driver_version = $CurrentDokanVersions | Where-Object{$_ -like "$ouisync_dokan_mayor.*"} | Sort-Object -Descending | Select-Object -First 1
+   
+    if ($dokan2_driver_version.Length -eq 0) {
+        # "Dokan $ouisync_dokan_mayor not found. Installation required (1)"
+        return 1
     }
 
-    # if ($MINOR -lt $ouisync_dokan_minor) {
-    #     return $true
-    # }
+    if ($dokan2_driver_version -lt $minimum_ouisync_dokan_version) {
+        # "Dokan $ouisync_dokan_mayor was found, but the driver is a lower version ($dokan2_driver_version) than the one used by Ouisync ($full_minimum_ouisync_dokan_version). An update is required, but we cannot do it automatically. (2)"
+        return 2
+    }
 
-    return $false
+    # The Dokan $ouisync_dokan_mayor version is equal or grather than required by Ouisync ($minimum_ouisync_dokan_version). No action needed.
+    return 0
 }
+
+# function Get-IsCurrentDokanVersionSupported {
+#     Param(
+#         [String] $DokanVersion
+#     )
+
+#     $version_array = $DokanVersion.replace(".", ":") -split ":"
+    
+#     $MAYOR = $version_array[0]
+#     $MINOR = $version_array[1]
+#     $PATCH = $version_array[2]
+
+#     if ("$MAYOR.$MINOR.$PATCH" -eq $minimum_ouisync_dokan_version -or
+#         $MAYOR -gt $ouisync_dokan_mayor) {
+#         return $true
+#     }
+
+#     # if ($MINOR -lt $ouisync_dokan_minor) {
+#     #     return $true
+#     # }
+
+#     return $false
+# }
 
 function Install-Dokan {
     Write-Host "Installing Dokan $full_minimum_ouisync_dokan_version"
@@ -70,9 +83,8 @@ function Install-Dokan {
 
 function Get-InstallDokanConfirmation {
     $title_dokan_installation = "Dokan installation required - Ouisync"
-    $message_dokan_installation = "Dokan is missing in this device.`n`nOuisync uses Dokan $full_minimum_ouisync_dokan_version for mounting unlocked repositories as drives, than later can be seen and opened in the File Explorer.`n`nWe can try to install it for you"
+    $message_dokan_installation = "Dokan $ouisync_dokan_mayor is missing in this device.`n`nOuisync uses Dokan $full_minimum_ouisync_dokan_version for mounting unlocked repositories as drives, than later can be seen and opened in the File Explorer.`n`nWe can try to install it for you"
 
-    Add-Type -AssemblyName System.Windows.Forms
     $result = [System.Windows.Forms.MessageBox]::Show($message_dokan_installation, $title_dokan_installation, [System.Windows.Forms.MessageBoxButtons]::OKCancel, [System.Windows.Forms.MessageBoxIcon]::Question, [System.Windows.Forms.MessageBoxDefaultButton]::Button1)
 
     if ($result -eq "Cancel") {
@@ -82,19 +94,40 @@ function Get-InstallDokanConfirmation {
     return $true
 }
 
-function Get-UpgradeDokanConfirmation {
-    $title_dokan_upgrade = "Dokan upgrade required - Ouisync"
-    $message_dokan_upgrade = "The Dokan version found ($dokan_version) is lower than required by Ouisync ($minimum_ouisync_dokan_version), and some features may not worked as expected.`n`nOuisync uses Dokan for mounting unlocked repositories as drives, than later can be seen and opened in the File Explorer.`n`nWe can try to upgrade it for you"
+function Install-Dokan2-WithConfirmation {
+    Write-Host "Dokan $ouisync_dokan_mayor is missing. Installing Dokan $full_minimum_ouisync_dokan_version"
 
-    Add-Type -AssemblyName System.Windows.Forms
-    $result = [System.Windows.Forms.MessageBox]::Show($message_dokan_upgrade, $title_dokan_upgrade, [System.Windows.Forms.MessageBoxButtons]::OKCancel, [System.Windows.Forms.MessageBoxIcon]::Question, [System.Windows.Forms.MessageBoxDefaultButton]::Button1)
-
-    if ($result -eq "Cancel") {
-        return $false
+    $confirmation_result = Get-InstallDokanConfirmation
+    if ($confirmation_result -eq $false) {
+        Write-Host "Dokan installation canceled by user"
+        return
     }
 
-    return $true
+    $install_result = Install-Dokan
+    if ($install_result -eq 0) {
+        Show-AlertDialog -Title "Dokan installation - Ouisync" -Message "Dokan $full_minimum_ouisync_dokan_version installed successfully."
+    } else {
+        Show-AlertDialog -Title "Dokan installation - Ouisync" -Message  "Dokan $full_minimum_ouisync_dokan_version installation failed. You may need to install Dokan manually for Ouisync to work properly.`n`nMinimum required version: $minimum_ouisync_dokan_version"
+    }
 }
+
+# function Get-UpgradeDokanConfirmation {
+#     Param(
+#         [String] $DokanVersion
+#     )
+
+#     $title_dokan_upgrade = "Dokan upgrade required - Ouisync"
+#     $message_dokan_upgrade = "The Dokan version found ($DokanVersion) is lower than required by Ouisync ($minimum_ouisync_dokan_version), and some features may not worked as expected.`n`nOuisync uses Dokan for mounting unlocked repositories as drives, than later can be seen and opened in the File Explorer.`n`nWe can try to upgrade it for you"
+
+#     Add-Type -AssemblyName System.Windows.Forms
+#     $result = [System.Windows.Forms.MessageBox]::Show($message_dokan_upgrade, $title_dokan_upgrade, [System.Windows.Forms.MessageBoxButtons]::OKCancel, [System.Windows.Forms.MessageBoxIcon]::Question, [System.Windows.Forms.MessageBoxDefaultButton]::Button1)
+
+#     if ($result -eq "Cancel") {
+#         return $false
+#     }
+
+#     return $true
+# }
 
 function Show-AlertDialog {
     Param(
@@ -109,46 +142,35 @@ function Show-AlertDialog {
 # OPERATIONS         #
 ######################
 
-$dokan_version = Get-DockanVersionInSystem
-if ($null -eq $dokan_version) {
-    Write-Host "Dokan is missing. Installing Dokan $full_minimum_ouisync_dokan_version"
+$dokan_version_list = Get-DokanVersionsInSystem
+Write-Host "Dokan versions found: $dokan_version_list"
 
-    $install_dokan_result = Get-InstallDokanConfirmation
-    if ($install_dokan_result -eq $false) {
-        Write-Host "Dokan installation canceled by user"
-        return
-    }
-
-    $result = Install-Dokan
-    if ($result -eq 0) {
-        Show-AlertDialog -Title "Dokan installation - Ouisync" -Message "Dokan $full_minimum_ouisync_dokan_version installed successfully."
-    } else {
-        Show-AlertDialog -Title "Dokan installation - Ouisync" -Message  "Dokan $full_minimum_ouisync_dokan_version installation failed. You may need to install Dokan manually for Ouisync to work properly.`n`nMinimum required version: $minimum_ouisync_dokan_version"
-    }
+if ($null -eq $dokan_version_list) {
+    Install-Dokan2-WithConfirmation
 
     return
 }
 
-# Dokan installation found. Checking the version in case of requiring update.
-Write-Host "Dokan version $dokan_version found"
+# Dokan found. Checking the version in case of requiring update.
+Write-Host "Checking the Dokan version required for Ouisync..."
 
-$supported = Get-IsCurrentDokanVersionSupported -DokanVersion $dokan_version
-if ($supported) {
-    Write-Host "The Dokan minimum required version for Ouisync is $minimum_ouisync_dokan_version.`n`nNo update is required (Current version: $dokan_version)"
-    return
-} 
+$installation_required = Get-IsDokanInstallRequired -CurrentDokanVersions $dokan_version_list
+if ($installation_required -eq 0) {
+    Write-Host "The Dokan driver version found is equal or greater than the one required by Ouisync ($minimum_ouisync_dokan_version). No action is required."
 
-Write-Host "The current version of Dokan ($DokanVersion) is lower than the minimum required for Ouisync ($minimum_ouisync_dokan_version).`n`nAn upgrade may be needed."
-
-$upgrade_dokan_result = Get-UpgradeDokanConfirmation
-if ($upgrade_dokan_result -eq $false) {
-    Write-Host "Dokan upgrade canceled by user"
     return
 }
 
-$dokan_upgrade_result = Install-Dokan
-if ($dokan_upgrade_result) {
-    Show-AlertDialog -Title "Dokan upgrade - Ouisync" -Message "Dokan upgraded successfully.`n`nWas $dokan_version; Current: $minimum_ouisync_dokan_version"
-} else {
-    Show-AlertDialog -Title "Dokan upgrade - Ouisync" -Message "Dokan upgrade failed. You may need to upgrade Dokan to version $minimum_ouisync_dokan_version manually for Ouisync to work properly."
+if ($installation_required -eq 1) {
+    Write-Host "Dokan $ouisync_dokan_mayor not found. Installation required."
+    Install-Dokan2-WithConfirmation
+
+    return
+}
+
+if ($installation_required -eq 2) {
+    Write-Host "Dokan $ouisync_dokan_mayor was found, but the driver is a lower version than the one used by Ouisync ($full_minimum_ouisync_dokan_version). An update is required, but we cannot do it automatically."
+    Show-AlertDialog -Title "Dokan installation - Ouisync" -Message "Dokan $ouisync_dokan_mayor was found, but the driver is a lower version than the one used by Ouisync ($full_minimum_ouisync_dokan_version). An update is required, but we cannot do it automatically."
+
+    return
 }
