4fcf508f333a6409a107a39bfebdb3cdd7125c59
Jorge Pabón <J-Pabon@users.noreply.github.com>
2024-03-13 09:59:46 +0800

    Update PowerShell script for installing Dokan: use msiexec directly instead of calling it using cmd

--- assets/install_dokan.ps1
+++ assets/install_dokan.ps1
@@ -1,13 +1,9 @@
 $msi_path = $args[0]
 
-# Write-Host "Installing Dokan
+$prog="msiexec.exe"
+$install_msi_params = "/i `"$msi_path`""
 
-$prog="cmd.exe"
-
-$install_driver_params=@("/C";"msiexec /i $msi_path";">C:\log_dokan_install_ouisync.txt")
-
-$process = Start-Process -Verb runas $prog $install_driver_params -Wait -PassThru
+$process = Start-Process $prog $install_msi_params -Wait -PassThru
 $cmd_result = $process.ExitCode
-# Write-Host "Dokan driver installation returned $cmd_result"
 
 return $cmd_result
