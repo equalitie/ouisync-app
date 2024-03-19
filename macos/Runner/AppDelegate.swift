c20614aae45cfc69437195b1672a19e320195c6f
Peter Jankuliak <inetic@gmail.com>
2024-03-19 14:09:29 +0100

    macos: start NSFileProviderExtension on app active

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,9 +1,21 @@
 import Cocoa
 import FlutterMacOS
+import AppKit
+import FileProvider
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
+  }
+
+  override func applicationDidBecomeActive(_ notification: Notification) {
+    let domain = NSFileProviderDomain(identifier: NSFileProviderDomainIdentifier(rawValue: "mydomain"), displayName: "mydisplayname")
+
+    NSFileProviderManager.add(domain, completionHandler: {error in
+      if let error = error {
+        print("Error starting file provider for domain \(domain): \(String(describing: error))")
+      }
+    })
   }
 }
