9223de8ce578e3a4717f6808f218992446835037
Jorge Pabón <J-Pabon@users.noreply.github.com>
2024-03-31 18:08:12 +0800

    Make the sort direction button a widget with the same style as the FAB

---
+++ lib/app/widgets/buttons/sort_direction_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../cubits/sort_list.dart';
+import '../../utils/dimensions.dart';
+
+class SortDirectionButton extends StatelessWidget {
+  const SortDirectionButton({
+    required this.direction,
+    required this.sortBy,
+    required this.sort,
+  });
+
+  final SortDirection direction;
+  final SortBy sortBy;
+  final void Function(SortDirection, SortBy) sort;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 3.0,
+      borderRadius: BorderRadius.all(Radius.circular(6.0)),
+      child: Center(
+        child: InkWell(
+          child: Container(
+            padding: EdgeInsets.all(6.0),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(Radius.circular(6.0)),
+            ),
+            child: _getDirectionArrow(direction),
+          ),
+          onTap: () => sort(direction, sortBy),
+        ),
+      ),
+    );
+  }
+
+  Widget _getDirectionArrow(SortDirection direction) {
+    return direction == SortDirection.asc
+        ? const Image(
+            image: AssetImage('assets/sort_asc.png'),
+            width: Dimensions.sizeIconMicro,
+          )
+        : const Image(
+            image: AssetImage('assets/sort_desc.png'),
+            width: Dimensions.sizeIconMicro,
+          );
+  }
+}
