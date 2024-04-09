67c257c700af02bf4b1ed79e4d9745e036cb1682
Jorge Pabón <J-Pabon@users.noreply.github.com>
2024-04-09 12:55:22 +0800

    Fix: local_auth plugin requires activity to be a FragmentActivity

--- android/app/src/main/kotlin/org/equalitie/ouisync/MainActivity.kt
+++ android/app/src/main/kotlin/org/equalitie/ouisync/MainActivity.kt
@@ -5,11 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
   companion object {
     private val CHANNEL = "org.equalitie.ouisync_app/native"
   }
