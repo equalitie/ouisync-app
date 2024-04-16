8d7f740b3e2da7641b1de654f623ee97aa9fa8e9
Adam Cigánek <adam.ciganek@proton.me>
2024-04-16 10:05:09 +0200

    Change product name (ouisync) and exe name (ouisync-gui.exe) on windows

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -50,11 +50,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "ouisync_app");
+    gtk_header_bar_set_title(header_bar, "Ouisync");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "ouisync_app");
+    gtk_window_set_title(window, "Ouisync");
   }
 
   gtk_window_set_default_size(window, 320, 200);//1280, 720);
