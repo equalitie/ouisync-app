ba50af7a99aefdc89ee7d10623e65da501bb0d33
Adam Cigánek <adam.ciganek@proton.me>
2024-04-17 08:35:10 +0200

    Fix migrate_files_test on windows, take 2

--- test/migrate_files_test.dart
+++ test/migrate_files_test.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:test/test.dart';
 import 'package:ouisync_app/app/utils/files.dart';
-import 'package:path/path.dart' hide equals;
+import 'package:path/path.dart' as p;
 
 // Run with `flutter test test/settings_test.dart`.
 void main() {
@@ -17,49 +17,70 @@ void main() {
   });
 
   test('migrateFiles sanity check', () async {
-    final src = Directory(join(temp.path, 'src'));
-    final dst = Directory(join(temp.path, 'dst'));
+    final src = Directory(p.join(temp.path, 'src'));
+    final dst = Directory(p.join(temp.path, 'dst'));
 
-    await File(join(src.path, 'a.txt')).create(recursive: true);
-    await File(join(src.path, 'dir/b.txt')).create(recursive: true);
-    await Directory(join(src.path, 'empty')).create(recursive: true);
-    await Link(join(src.path, 'inside-rel')).create('a.txt');
-    await Link(join(src.path, 'inside-abs')).create(join(src.path, 'a.txt'));
-    await Link(join(src.path, 'outside-rel')).create('../c.txt');
-    await Link(join(src.path, 'outside-abs')).create(join(temp.path, 'c.txt'));
+    await File(p.join(src.path, 'a.txt')).create(recursive: true);
+    await File(p.join(src.path, 'dir/b.txt')).create(recursive: true);
+    await Directory(p.join(src.path, 'empty')).create(recursive: true);
+    await Link(p.join(src.path, 'inside-rel')).create('a.txt');
+    await Link(p.join(src.path, 'inside-abs'))
+        .create(p.join(src.path, 'a.txt'));
+    await Link(p.join(src.path, 'outside-rel')).create('../c.txt');
+    await Link(p.join(src.path, 'outside-abs'))
+        .create(p.join(temp.path, 'c.txt'));
 
     await migrateFiles(src, dst);
 
     expect(
-        await _collectFiles(dst),
+        await _collectContent(dst),
         equals([
-          ('a.txt', null),
-          ('dir', null),
-          (join('dir', 'b.txt'), null),
-          ('empty', null),
-          ('inside-abs', join(dst.path, 'a.txt')),
-          ('inside-rel', 'a.txt'),
-          ('outside-abs', join(temp.path, 'c.txt')),
-          ('outside-rel', join(temp.path, 'c.txt')),
+          Entity('a.txt'),
+          Entity('dir'),
+          Entity(p.join('dir', 'b.txt')),
+          Entity('empty'),
+          Entity('inside-abs', p.join(dst.path, 'a.txt')),
+          Entity('inside-rel', 'a.txt'),
+          Entity('outside-abs', p.join(temp.path, 'c.txt')),
+          Entity('outside-rel', p.join(temp.path, 'c.txt')),
         ]));
 
     expect(await src.exists(), isFalse);
   });
 }
 
-Future<List<(String, String?)>> _collectFiles(
+class Entity {
+  final String path;
+  final String target;
+
+  Entity(this.path, [this.target = '']);
+
+  @override
+  bool operator ==(Object other) =>
+      other is Entity &&
+      p.equals(path, other.path) &&
+      p.equals(target, other.target);
+
+  @override
+  int get hashCode => p.hash(path) ^ p.hash(target);
+
+  @override
+  String toString() => target.isNotEmpty ? '$path -> $target' : path;
+}
+
+Future<List<Entity>> _collectContent(
   Directory dir,
 ) async {
   final content = await dir
       .list(recursive: true, followLinks: false)
       .asyncMap(
-        (e) async => (
-          relative(e.path, from: dir.path),
-          (e is Link) ? await e.target() : null
+        (e) async => Entity(
+          p.relative(e.path, from: dir.path),
+          (e is Link) ? await e.target() : '',
         ),
       )
       .toList();
-  content.sort((a, b) => a.$1.compareTo(b.$1));
+  content.sort((a, b) => a.path.compareTo(b.path));
 
   return content;
 }
