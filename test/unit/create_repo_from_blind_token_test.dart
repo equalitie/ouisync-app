ddff9eb62dd6ae09b3ddc64696cc0123b229d50b
Adam Cigánek <adam.ciganek@proton.me>
2024-07-25 16:15:39 +0200

    Simplify create_repo_from_blind_token_test

--- test/unit/create_repo_from_blind_token_test.dart
+++ test/unit/create_repo_from_blind_token_test.dart
@@ -1,73 +1,40 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ouisync/native_channels.dart';
 import 'package:ouisync/ouisync.dart';
-import 'package:ouisync_app/app/cubits/cubits.dart';
 import 'package:ouisync_app/app/cubits/repo_creation.dart';
 import 'package:ouisync_app/app/models/models.dart';
-import 'package:ouisync_app/app/utils/master_key.dart';
-import 'package:ouisync_app/app/utils/repo_path.dart' as repo_path;
 import 'package:ouisync_app/app/utils/share_token.dart';
-import 'package:ouisync_app/app/utils/utils.dart';
-import 'package:path_provider/path_provider.dart';
 
 import '../utils.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-  late Session session;
-  late ReposCubit reposCubit;
+  late TestDependencies deps;
   late RepoCreationCubit repoCreationCubit;
 
   final String tokenString =
       'https://ouisync.net/r#AwAgTmm8jPQuimEWzqVcM17M2-3GXIULZQ1tbQRVsBgj9PY?name=uno';
 
   setUp(() async {
-    final configPath = repo_path.join(
-      (await getApplicationSupportDirectory()).path,
-      'config',
-    );
-
-    session = Session.create(
-      kind: SessionKind.unique,
-      configPath: configPath,
-    );
-
-    final nativeChannels = NativeChannels(session);
-    final settings = await Settings.init(MasterKey.random());
-
-    reposCubit = ReposCubit(
-      session: session,
-      nativeChannels: nativeChannels,
-      settings: settings,
-      navigation: NavigationCubit(),
-      bottomSheet: EntryBottomSheetCubit(),
-      cacheServers: CacheServers.disabled,
-    );
-
-    repoCreationCubit = RepoCreationCubit(reposCubit: reposCubit);
+    deps = await TestDependencies.create();
+    repoCreationCubit = RepoCreationCubit(reposCubit: deps.reposCubit);
   });
 
   tearDown(() async {
     await repoCreationCubit.close();
-    await reposCubit.close();
-    await session.close();
+    await deps.dispose();
   });
 
   test('create repository with blind token', () async {
-    final token = await parseShareToken(reposCubit, tokenString);
+    final token = await parseShareToken(deps.reposCubit, tokenString);
     expect(
         token,
         isA<ShareTokenValid>()
-            .having((t) => t.value, 'token', isNotNull)
+            .having((t) => t.value, 'value', isNotNull)
             .having((t) => t.error, 'error', isNull));
 
     final tokenAccesMode = await (token as ShareTokenValid).value.mode;
-    expect(
-        tokenAccesMode,
-        isA<AccessMode>()
-            .having((t) => t.name, 'mode', equals(AccessMode.blind.name)));
+    expect(tokenAccesMode, equals(AccessMode.blind));
 
     final suggestedRepoName = await token.value.suggestedName;
 
@@ -80,33 +47,27 @@ void main() {
           .having((s) => s.nameError, 'nameError', isNull),
     );
 
-    repoCreationCubit.nameController.text = suggestedRepoName;
-
-    await repoCreationCubit
-        .waitUntil((state) => state.substate is RepoCreationValid);
-    expect(repoCreationCubit.state.name, equals(suggestedRepoName));
-
     await repoCreationCubit.setToken(token.value);
 
+    repoCreationCubit.nameController.text = suggestedRepoName;
+    await repoCreationCubit.waitUntil((state) => !state.loading);
+
+    expect(repoCreationCubit.state.substate, isA<RepoCreationValid>());
+    expect(repoCreationCubit.state.name, equals(suggestedRepoName));
     expect(
-        repoCreationCubit.state.localSecretMode,
-        isA<LocalSecretMode>().having((lsm) => lsm.name, 'local_secret_mode',
-            LocalSecretMode.manual.name));
+      repoCreationCubit.state.localSecretMode,
+      equals(LocalSecretMode.manual),
+    );
 
     await repoCreationCubit.save();
 
-    await repoCreationCubit
-        .waitUntil((state) => state.substate is RepoCreationSuccess);
-
-    await repoCreationCubit.save();
     expect(repoCreationCubit.state.substate, isA<RepoCreationSuccess>());
     expect(
-      reposCubit.repos
+      deps.reposCubit.repos
           .where((entry) => entry.name == suggestedRepoName)
           .firstOrNull,
       isA<OpenRepoEntry>()
-          .having((e) => e.accessMode, 'mode', AccessMode.blind)
-          .having((e) => e.cubit.accessMode, 'mode', AccessMode.blind),
+          .having((e) => e.accessMode, 'accessMode', AccessMode.blind),
     );
   });
 }
