3d55685f624dcaf5d91f17f3b4aa2ea9637c8667
Jorge Pabón <J-Pabon@users.noreply.github.com>
2024-09-09 09:49:52 +0800

    Add language picker page

---
+++ lib/app/pages/language_picker_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LanguagePickerPage extends StatefulWidget {
+  const LanguagePickerPage({super.key});
+
+  @override
+  State<LanguagePickerPage> createState() => _LanguagePickerPageState();
+}
+
+class _LanguagePickerPageState extends State<LanguagePickerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
