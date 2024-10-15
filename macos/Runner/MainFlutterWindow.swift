78cfe09c226c39d3a0685046f753c74b66078323
Radu Dan <radu@full-throttle.ro>
2024-10-15 20:22:28 +0300

    Implement launch at startup
    This adds the missing glue code that the macOS version of the
    [`launch_at_startup`](https://pub.dev/packages/launch_at_startup)
    library requires.

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,6 +2,8 @@ import Cocoa
 import FlutterMacOS
 import Common
 import FileProvider
+import LaunchAtLogin
+
 
 class MainFlutterWindow: NSWindow {
     var fileProviderProxy: FileProviderProxy? = nil
@@ -17,10 +19,32 @@ class MainFlutterWindow: NSWindow {
         let flutterBinaryMessenger = flutterViewController.engine.binaryMessenger
         setupFlutterToExtensionProxy(flutterBinaryMessenger)
         setupFlutterMethodChannel(flutterBinaryMessenger)
+        setupFlutterAutostartChannel(flutterBinaryMessenger)
 
         RegisterGeneratedPlugins(registry: flutterViewController)
 
         super.awakeFromNib()
+    }
+
+    // ------------------------------------------------------------------
+    // Autostart requires some custom platform integration as per:
+    // https://pub.dev/packages/launch_at_startup#macos-support
+    // ------------------------------------------------------------------
+    fileprivate func setupFlutterAutostartChannel(_ binaryMessenger: FlutterBinaryMessenger) {
+        FlutterMethodChannel(name: "launch_at_startup",
+                             binaryMessenger: binaryMessenger)
+        .setMethodCallHandler { call, result in
+            switch call.method {
+            case "launchAtStartupSetEnabled":
+                if let arguments = call.arguments as? [String: Any],
+                   let value = arguments["setEnabledValue"] as? Bool {
+                    LaunchAtLogin.isEnabled = value
+                }
+                fallthrough
+            case "launchAtStartupIsEnabled": result(LaunchAtLogin.isEnabled)
+            default: result(FlutterMethodNotImplemented)
+            }
+        }
     }
 
     // ------------------------------------------------------------------
