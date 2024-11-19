66d74ce48dfe9fba6419f916897355c3992bf31c
Jorge Pabón <J-Pabon@users.noreply.github.com>
2024-11-19 10:06:32 +0800

    Simplify test

--- test/widget/move_entry_repo_no_write_test.dart
+++ test/widget/move_entry_repo_no_write_test.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
-import 'package:ouisync_app/app/cubits/repo.dart';
-import 'package:ouisync_app/app/models/models.dart';
+import 'package:ouisync_app/app/cubits/cubits.dart' show RepoCubit;
+import 'package:ouisync_app/app/models/models.dart'
+    show LocalSecretMode, LocalSecretKeyAndSalt, RepoEntry, RepoLocation;
 import 'package:ouisync/ouisync.dart' show AccessMode, File, Repository;
 
-import '../utils.dart'
-    show BlocBaseExtension, TestDependencies, WidgetTesterExtension, testApp;
+import '../utils.dart' show BlocBaseExtension, TestDependencies, testApp;
 
 void main() {
   late TestDependencies deps;
 
-  late String originRepoName = 'origin';
-  late String destinationName = 'destination';
+  final String originRepoName = 'origin';
+  final String blindRepoName = 'destination';
+
+  late Repository originRepo;
+  late RepoEntry blindRepoEntry;
 
   setUp(() async {
     deps = await TestDependencies.create();
 
     final reposDir = await deps.reposCubit.settings.getDefaultRepositoriesDir();
 
-    originRepoName = 'origin';
     final originRepoLocation = RepoLocation.fromParts(
       dir: reposDir,
       name: originRepoName,
     );
 
-    destinationName = 'destination';
     final blindRepoLocation = RepoLocation.fromParts(
       dir: reposDir,
-      name: destinationName,
+      name: blindRepoName,
     );
 
     await deps.reposCubit.createRepository(
@@ -37,13 +38,15 @@ void main() {
       localSecretMode: LocalSecretMode.randomStored,
     );
 
-    await deps.reposCubit.createRepository(
+    blindRepoEntry = await deps.reposCubit.createRepository(
       location: blindRepoLocation,
       setLocalSecret: LocalSecretKeyAndSalt.random(),
       localSecretMode: LocalSecretMode.randomStored,
     );
 
-    final originRepo = await Repository.open(
+    await blindRepoEntry.cubit?.lock();
+
+    originRepo = await Repository.open(
       deps.session,
       store: originRepoLocation.path,
     );
@@ -54,11 +57,13 @@ void main() {
   });
 
   tearDown(() async {
+    await blindRepoEntry.close();
+    await originRepo.close();
     await deps.dispose();
   });
 
   testWidgets(
-    'move file to a repo and confirm that MOVE button is disabled when locked',
+    'move file to a blind/locked repo and confirm that MOVE button is disabled',
     (tester) => tester.runAsync(
       () async {
         await loadAppFonts();
@@ -67,25 +72,25 @@ void main() {
         await tester.pumpAndSettle();
 
         expect(find.text(originRepoName), findsOne);
-        expect(find.text(destinationName), findsOne);
+        expect(find.text(blindRepoName), findsOne);
 
-        final originRepo = deps.reposCubit.repos
+        final originRepoCubit = deps.reposCubit.repos
             .firstWhere((r) => r.name == originRepoName)
             .cubit!;
 
         await tester.tap(find.text(originRepoName));
-        await _waitForNavigationIntoRepoToEnd(deps, tester, originRepo);
+        await _waitForNavigationIntoRepoToEnd(deps, tester, originRepoCubit);
 
-        final vert = find.byKey(ValueKey('file_vert'));
-        expect(vert, findsOne);
+        final fileVert = find.byKey(ValueKey('file_vert'));
+        expect(fileVert, findsOne);
 
-        await tester.tap(vert);
+        await tester.tap(fileVert);
         await tester.pumpAndSettle();
 
-        final move = find.widgetWithText(ListTile, 'Move');
-        expect(move, findsOne);
+        final moveListTile = find.widgetWithText(ListTile, 'Move');
+        expect(moveListTile, findsOne);
 
-        await tester.tap(move);
+        await tester.tap(moveListTile);
         await tester.pumpAndSettle();
 
         final backButton = find.widgetWithIcon(
@@ -97,52 +102,26 @@ void main() {
         await tester.tap(backButton);
         await tester.pumpAndSettle();
 
-        final destinationRepo = deps.reposCubit.repos
-            .firstWhere((r) => r.name == destinationName)
+        final destinationRepoCubit = deps.reposCubit.repos
+            .firstWhere((r) => r.name == blindRepoName)
             .cubit!;
 
-        await tester.tap(find.text(destinationName));
-        await _waitForNavigationIntoRepoToEnd(deps, tester, destinationRepo);
+        await tester.tap(find.text(blindRepoName));
+        await _waitForNavigationIntoRepoToEnd(
+          deps,
+          tester,
+          destinationRepoCubit,
+        );
 
-        await tester.takeScreenshot('repo_blind0');
-
-        expect(destinationRepo.state.accessMode, equals(AccessMode.write));
-
-        final lockButton = find.byKey(ValueKey('lock_repo_icon'));
-        expect(lockButton, findsOne);
-
-        await tester.tap(lockButton);
-
-        await deps.reposCubit.currentRepo?.cubit?.waitUntil((_) =>
-            deps.reposCubit.currentRepo?.cubit?.state.accessMode ==
-            AccessMode.blind);
-        await tester.pumpAndSettle();
-        await tester.pump(Duration(seconds: 1));
-
-        await tester.takeScreenshot('repo_blind1');
-
-        expect(destinationRepo.state.accessMode, equals(AccessMode.blind));
-        expect(
-            deps.reposCubit.currentRepo?.accessMode, equals(AccessMode.blind));
-
-        await tester.pumpAndSettle();
-
-        await tester.tap(backButton);
-        await tester.pumpAndSettle();
-
-        await tester.takeScreenshot('repo_blind2');
-
-        await tester.tap(find.text(destinationName));
-        await tester.pump(Duration(seconds: 1));
-
-        await _waitForNavigationIntoRepoToEnd(deps, tester, destinationRepo);
-
-        await tester.takeScreenshot('repo_blind3');
+        final currentRepoEntry = deps.reposCubit.currentRepo;
+        expect(currentRepoEntry?.accessMode, equals(AccessMode.blind));
 
         final moveButton = find.descendant(
-            of: find.byKey(ValueKey('move_entry')),
-            matching: find
-                .byWidgetPredicate((widget) => widget is RawMaterialButton));
+          of: find.byKey(ValueKey('move_entry')),
+          matching: find.byWidgetPredicate(
+            (widget) => widget is RawMaterialButton,
+          ),
+        );
         expect(moveButton, findsOne);
         expect(tester.widget<RawMaterialButton>(moveButton).enabled, false);
       },
