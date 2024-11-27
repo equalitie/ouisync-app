7c2387fb97ce3a7ca4f92378de277a2a2b65cf0e
Peter Jankuliak <p.jankuliak@gmail.com>
2024-11-27 18:56:04 +0100

    Print exception sometimes thrown on CI

--- test/utils.dart
+++ test/utils.dart
@@ -90,6 +90,9 @@ Future<void> testEnv(FutureOr<void> Function() callback) async {
       // This sometimes happen on the CI on windows. It seems to be caused by another process
       // accessing the temp directory for some reason. It probably doesn't indicate a problem in
       // the code under test so it should be safe to ignore it.
+    } catch (exception) {
+      print("Exception during temporary directory removal: $exception");
+      rethrow;
     }
   });
 
