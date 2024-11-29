c2b7dc9084d9bc93d26060d6ff4630757bc338c1
Peter Jankuliak <p.jankuliak@gmail.com>
2024-11-29 13:44:29 +0100

    Remove dead code

--- lib/app/pages/repo_security_page.dart
+++ lib/app/pages/repo_security_page.dart
@@ -80,8 +80,7 @@ class _State extends State<RepoSecurityPage> {
               _onPopInvoked(context, didPop, cubit.state),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            if (currentLocalSecret != null)
-              RepoSecurityWidget(cubit, isBlind: false),
+            if (currentLocalSecret != null) RepoSecurityWidget(cubit),
             // TODO: Arbitrary size, this can likely be done better.
             SizedBox(height: 14),
             _buildResetRepoUsingTokenButton(context),
@@ -203,17 +202,10 @@ class _State extends State<RepoSecurityPage> {
 class RepoSecurityWidget extends StatelessWidget {
   const RepoSecurityWidget(
     this.cubit, {
-    required this.isBlind,
-    this.repoName,
     super.key,
   });
 
   final RepoSecurityCubit cubit;
-
-  // If a repository is blind, the security options are disabled, and a message
-  // is shown explaining this.
-  final bool isBlind;
-  final String? repoName;
 
   @override
   Widget build(BuildContext context) {
@@ -225,35 +217,12 @@ class RepoSecurityWidget extends StatelessWidget {
       builder: (context, state) => Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          _buildRepoName(context, repoName),
           _buildOriginSwitch(state),
           _buildPasswordFields(state),
           _buildStoreSwitch(state),
           _buildSecureWithBiometricsSwitch(state),
-          _buildDisabledOptionsMessage(isBlind, warningStyle),
           _buildManualPasswordWarning(state, warningStyle),
         ],
-      ),
-    );
-  }
-
-  Widget _buildRepoName(BuildContext context, String? repoName) {
-    if (repoName == null) return const SizedBox.shrink();
-
-    final titleFontSize = context.theme.textTheme.titleLarge?.fontSize;
-    final textStyle = context.theme.textTheme.bodyMedium?.copyWith(
-        fontSize: titleFontSize,
-        fontWeight: FontWeight.w400,
-        textBaseline: TextBaseline.ideographic);
-
-    return Container(
-      padding: Dimensions.paddingVertical10,
-      child: ScrollableTextWidget(
-        child: Text(
-          '\'$repoName\'',
-          style: textStyle,
-        ),
-        fontSize: titleFontSize,
       ),
     );
   }
@@ -271,10 +240,8 @@ class RepoSecurityWidget extends StatelessWidget {
         key: Key('use-local-password'), // Used in tests
         value: state.plannedOrigin == SecretKeyOrigin.manual,
         title: S.current.messageUseLocalPassword,
-        onChanged: isBlind
-            ? null
-            : (value) => cubit.setOrigin(
-                value ? SecretKeyOrigin.manual : SecretKeyOrigin.random),
+        onChanged: (value) => cubit
+            .setOrigin(value ? SecretKeyOrigin.manual : SecretKeyOrigin.random),
       );
 
   Widget _buildStoreSwitch(RepoSecurityState state) =>
@@ -298,23 +265,11 @@ class RepoSecurityWidget extends StatelessWidget {
           ? _buildSwitch(
               value: state.plannedWithBiometrics.toBool,
               title: S.current.messageSecureUsingBiometrics,
-              onChanged: isBlind
-                  ? null
-                  : state.isSecureWithBiometricsEnabled
-                      ? cubit.setSecureWithBiometrics
-                      : null,
+              onChanged: state.isSecureWithBiometricsEnabled
+                  ? cubit.setSecureWithBiometrics
+                  : null,
             )
           : SizedBox.shrink();
-
-  Widget _buildDisabledOptionsMessage(
-    bool visible,
-    TextStyle warningStyle,
-  ) =>
-      _buildWarning(
-        visible,
-        S.current.messageSecurityOptionsNotAvailableBlind,
-        warningStyle,
-      );
 
   Widget _buildManualPasswordWarning(
     RepoSecurityState state,
