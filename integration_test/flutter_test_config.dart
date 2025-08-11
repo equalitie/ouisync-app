be728ab3d61565e578ab3bc3f1657f018d764265
Adam Cigánek <adam.ciganek@proton.me>
2025-08-11 12:46:13 +0200

    Setup separate sandbox per integration test

--- integration_test/flutter_test_config.dart
+++ integration_test/flutter_test_config.dart
@@ -2,18 +2,9 @@ import 'dart:async';
 
 import 'package:ouisync_app/app/utils/flavor.dart';
 
-import '../test/sandbox.dart';
-
 /// See the "Per directory hierarchy" section in
 /// https://api.flutter.dev/flutter/flutter_test/flutter_test-library.html for more info.
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   Flavor.current = Flavor.itest;
-
-  final sandbox = await Sandbox.setUp();
-
-  try {
-    await testMain();
-  } finally {
-    await sandbox.tearDown();
-  }
+  await testMain();
 }
